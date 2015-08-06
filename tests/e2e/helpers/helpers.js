61a3d20e0e92ddd0e5f1f901c93058abc6e537b1
Renato Vasconcellos <revagomes@gmail.com>
2015-08-06 10:44:53 -0300

    Added the missing helpers file for the tests.

---
+++ tests/e2e/helpers/helpers.js
@@ -0,0 +1,58 @@
+//
+// Helper Funcions
+//
+
+var path = require('path');
+
+// Used for non-angular apps
+browser.ignoreSynchronization = true;
+
+function selectDropdownbyNum(element, optionNum) {
+  if (optionNum) {
+    var options = element.findElements(by.tagName('option'))
+      .then(function(options){
+        options[optionNum].click();
+      });
+  }
+}
+
+function selectAutocompleteReference(element, text) {
+  signatureInput = element;
+  signatureInput.click()
+  signatureInput.clear()
+  signatureInput.sendKeys(text);
+  browser.sleep(1000);
+  element(by.cssContainingText('.reference-autocomplete', text)).click();
+}
+
+function clickElement(id) {
+  return element(by.id(id)).click();
+}
+
+function addMedia(mediaButtonId, mediaFile) {
+  // Click on media browse button.
+  // clickElement(mediaButtonId);
+
+  return browser.switchTo().frame('mediaBrowser').then(function() {
+    var mediaElement = element.all(by.id('edit-upload-upload')).last()
+      , nextButton = element(by.css('#edit-next'))
+      , saveButton = element(by.css('#edit-submit'))
+      , mediaInput = path.resolve(__dirname, '../assets/' + mediaFile);
+
+    // Upload media.
+    return mediaElement.sendKeys(mediaInput).then(function() {
+      return nextButton.click().then(function() {
+        return saveButton.click().then(function() {
+          browser.sleep(1000);
+          return browser.switchTo().defaultContent();
+        });
+      });
+    });
+  });
+}
+
+// Exposed helper methods.
+module.exports = {
+  clickElement: clickElement
+  , addMedia: addMedia
+}
