35cee765d16a5b0540fa69b9ffb0dcf9f4e927f8
Renato Vasconcellos <revagomes@gmail.com>
2015-08-10 22:42:45 -0300

    Fixed typo in tests artifacts directory.

--- tests/e2e/specs/artifacts/artifact.fieldnote.spec.js
+++ tests/e2e/specs/artifacts/artifact.fieldnote.spec.js
@@ -9,6 +9,9 @@ var EC = protractor.ExpectedConditions;
 // For each spec file is recommended to have just one describe.
 // A describe may the the description of a functionality/feature or even a web page, like home page, contact page, etc. It depends on the team work agreement
 describe ('Fieldnote' , function () {
+
+  var userEmail = 'foo@bar.baz';
+
   // This is the pre-condition step of each test.
   beforeEach(function () {
     AllPages.AuthenticationPage.logout();
@@ -33,4 +36,21 @@ describe ('Fieldnote' , function () {
     browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
     expect(AllPages.SamplePage.body.getText()).toContain('has been created.');
   });
+
+  // it ('Add a Fieldnote as a contributor user', function () {
+  //   AllPages.AuthenticationPage.logout();
+  //   AllPages.RegistrationPage.register('foo', userEmail, browser.params.admin.password, true);
+  //   AllPages.AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
+  //   AllPages.PeoplePage.unblock(userEmail);
+  //   AllPages.PeoplePage.addRole(userEmail, 'Contributor');
+  //   AllPages.PeoplePage.addRole(userEmail, 'Researcher');
+  //   AllPages.AuthenticationPage.logout();
+  //   AllPages.AuthenticationPage.login('foo', browser.params.admin.password);
+  //   AllPages.FieldnotePage.get();
+  //   AllPages.FieldnotePage.add('Fieldnote text');
+  //   expect(AllPages.SamplePage.body.getText()).toContain('has been created.');
+  //   AllPages.AuthenticationPage.logout();
+  //   AllPages.AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
+  //   AllPages.PeoplePage.deleteUser(userEmail);
+  // });
 });
