99031195f54868a374bf9971b2ad48de47f3ac91
wlsf82 <wlsf82@gmail.com>
2015-08-12 17:38:16 -0300

    Refactoring on artifact fieldnote tests to use checkMessage method from sample page object.

--- tests/e2e/specs/artifacts/artifact.fieldnote.spec.js
+++ tests/e2e/specs/artifacts/artifact.fieldnote.spec.js
@@ -4,7 +4,6 @@
 
 // Require fieldnote page object.
 var AllPages = require('../../pages/all.page');
-var EC = protractor.ExpectedConditions;
 
 // For each spec file is recommended to have just one describe.
 // A describe may the the description of a functionality/feature or even a web page, like home page, contact page, etc. It depends on the team work agreement
@@ -21,8 +20,7 @@ describe ('Fieldnote' , function () {
   afterAll(function () {
     AllPages.ContentPage.get();
     AllPages.ContentPage.remove('pece_artifact_fieldnote');
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('has been deleted.');
+    AllPages.SamplePage.checkMessage('has been deleted.');
   });
 
   it ('verify main elements presence', function () {
@@ -33,8 +31,7 @@ describe ('Fieldnote' , function () {
   it ('add a fieldnote artifact', function () {
     AllPages.FieldnotePage.get();
     AllPages.FieldnotePage.add('Fieldnote text');
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('has been created.');
+    AllPages.SamplePage.checkMessage('has been created.');
   });
 
   // it ('Add a Fieldnote as a contributor user', function () {
