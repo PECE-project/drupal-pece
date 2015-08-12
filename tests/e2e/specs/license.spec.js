4e2287bf8ecb2baa23c22854c3483594f4b8a746
wlsf82 <wlsf82@gmail.com>
2015-08-12 18:24:27 -0300

    Changed license test descriptions and added @TODOS to it.

--- tests/e2e/specs/license.spec.js
+++ tests/e2e/specs/license.spec.js
@@ -12,16 +12,19 @@ describe ('License' , function () {
     AllPages.AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
   });
 
-  it ('should check the field_pece_license is present for Artifact content type', function () {
+  it ('check the field_pece_license presence and its default value for Artifact content type', function () {
     AllPages.PeceArtifactPage.get();
     expect(AllPages.PeceArtifactPage.licenseField.isPresent()).toBe(true);
     expect(AllPages.PeceArtifactPage.licenseFieldDefaultValue.getText()).toEqual('Attribution, Share Alike CC BY-SA');
   });
 
-  it ('should be able to inherit the artifact fields', function () {
+  // @TODO: I think it could be in another file, since this is not directly related with Licence.
+  it ('can inherit Artifact\'s fields', function () {
     AllPages.AddTypesPage.get();
     AllPages.AddTypesPage.inheritanceLink.click();
     AllPages.AddTypesPage.inheritanceCheckBox.click();
+    // @TODO: Today we already have two more artifacts with the word Artifact, beyond the one that we need
+    // to check, like Artifact - Image and Artifact - PDF Document, so, this is a not reliable test.
     expect(AllPages.AddTypesPage.bundleInheritParentSelect.getText()).toContain('Artifact');
   });
 });
