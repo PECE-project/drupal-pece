b66f0c41ea452984a9b77a7e6c4a066f506bcae5
wlsf82 <wlsf82@gmail.com>
2015-08-12 17:07:58 -0300

    Added get function to legal page, removed wrong comment and added missing semi colon.

--- tests/e2e/pages/legal.page.js
+++ tests/e2e/pages/legal.page.js
@@ -4,14 +4,17 @@
 
 var LegalPage = function () {
 
-  // Define sample attributes.
   this.tosTextarea = element(by.css('textarea#edit-conditions'));
   this.submitButton = element(by.css('input#edit-save'));
+
+  this.get = function () {
+    browser.get('admin/config/people/legal');
+  };
 
   this.create = function () {
     this.tosTextarea.sendKeys('Lorem Ipsum');
     this.submitButton.click();
-  }
+  };
 };
 
 module.exports = new LegalPage();
