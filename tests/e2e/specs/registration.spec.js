a8cbe19300a143a2eb281fb844291a12941f487b
wlsf82 <wlsf82@gmail.com>
2015-08-12 17:12:15 -0300

    Updated registration spec file to use get method from legal page object on beforeAll function and to use check message method from sample page object.

--- tests/e2e/specs/registration.spec.js
+++ tests/e2e/specs/registration.spec.js
@@ -4,7 +4,6 @@
 
 // Require authentication page object.
 var AllPages= require('../pages/all.page');
-var EC = protractor.ExpectedConditions;
 
 // For each spec file is recommended to have just one describe.
 // A describe may the the description of a functionality/feature or even a web page, like home page, contact page, etc. It depends on the team work agreement
@@ -12,7 +11,7 @@ describe ('Registration' , function () {
   beforeAll(function () {
     AllPages.AuthenticationPage.logout();
     AllPages.AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
-    AllPages.SamplePage.get('admin/config/people/legal');
+    AllPages.LegalPage.get();
     AllPages.LegalPage.create();
   });
 
@@ -21,28 +20,25 @@ describe ('Registration' , function () {
   });
 
   afterAll(function () {
+    AllPages.AuthenticationPage.logout();
     AllPages.AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
     AllPages.PeoplePage.deleteUser('foobar@bar.baz');
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('has been deleted.');
+    AllPages.SamplePage.checkMessage('has been deleted.');
   });
 
   it ('should be not accomplished', function () {
     AllPages.RegistrationPage.register('foo', 'foo@bar.baz', browser.params.admin.password, false);
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('Accept Terms & Conditions of Use field is required.');
+    AllPages.SamplePage.checkMessage('Accept Terms & Conditions of Use field is required.');
   });
 
   it ('invalid email on user registration', function () {
     var invalidEmail = 'bar';
     AllPages.RegistrationPage.register('bar', invalidEmail, browser.params.admin.password, true);
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('The e-mail address ' + invalidEmail +  ' is not valid.');
+    AllPages.SamplePage.checkMessage('The e-mail address ' + invalidEmail +  ' is not valid.');
   });
 
   it ('should be succesfuly done', function () {
     AllPages.RegistrationPage.register('foobar', 'foobar@bar.baz', browser.params.admin.password, true);
-    browser.wait(EC.visibilityOf(AllPages.SamplePage.body), browser.params.timeoutLimit);
-    expect(AllPages.SamplePage.body.getText()).toContain('Your account is currently pending approval by the site administrator.');
+    AllPages.SamplePage.checkMessage('Your account is currently pending approval by the site administrator.');
   });
 });
