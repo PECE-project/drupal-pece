a18ff3185552debd33a41f6d7c851dc01f64686e
Renato Vasconcellos <revagomes@gmail.com>
2015-08-18 17:08:50 -0300

    Added authentication.example.js file for automated tests.

---
+++ tests/e2e/authentication.example.js
@@ -0,0 +1,23 @@
+/**
+ * @file authentication.js
+ */
+
+/**
+ * Define user credentials
+ */
+var userInfo = {
+    user: 'admin',
+    password: 'impossiblepassword'
+};
+
+function getInfo (info) {
+    return userInfo[info];
+}
+
+module.exports = {
+    // Expose functions
+    getInfo: getInfo,
+
+    // Expose static attribues.
+    userInfo: userInfo
+};
