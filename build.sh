054e20ba7f4cd4541e885674d9fe631b7fc6d648
Sebastian Ferrari <sebas@taller.net.br>
2015-08-25 20:47:39 -0300

    refactorings in scripts and dot make.

--- build.sh
+++ build.sh
@@ -5,30 +5,13 @@ cd build
 PASSWORD=${PECE_DRUPAL_ADMIN_PASS:-impossiblepassword}
 # drush kw-id
 drush si pece --site-name="PECE Drupal Distro" --account-pass=${PASSWORD} -y
-drush kw-m
-drush cc all
+drush kw-u
 ## Uncomment for CM left sync.
 # drush clsyn
 
-if [ -d "profiles/pece/themes/taller/assets/sass/" ]; then
+if [ -d "profiles/pece/themes/pece_radix/" ]; then
   (
-    cd profiles/pece/themes/taller/assets/sass/
-    bundle install
-    bundle exec compass compile
-  )
-fi
-
-if [ -d "profiles/pece/themes/pece_radix/assets/sass/" ]; then
-  (
-    cd profiles/pece/themes/pece/assets/sass/
-    bundle install
-    bundle exec compass compile
-  )
-fi
-
-if [ -d "profiles/pece/themes/contrib/ember" ]; then
-  (
-    cd profiles/pece/themes/contrib/ember
+    cd profiles/pece/themes/pece_radix/
     bundle install
     bundle exec compass compile
   )
--- update.sh
+++ update.sh
@@ -1,21 +1,7 @@
 cd ./build
-drush updb -y
-drush fra -y
-drush kw-m
-if [ -d "profiles/pece/themes/taller/assets/sass/" ]; then
-  cd profiles/pece/themes/taller/assets/sass/
-  bundle install
-  bundle exec compass compile
-fi
-cd ~/pece/build
-if [ -d "profiles/pece/themes/pece/assets/sass/" ]; then
-  cd profiles/pece/themes/pece/assets/sass/
-  bundle install
-  bundle exec compass compile
-fi
-cd ~/pece/build
-if [ -d "profiles/pece/themes/ember" ]; then
-  cd profiles/pece/themes/ember
+drush kw-u
+if [ -d "profiles/pece/themes/pece_radix/" ]; then
+  cd profiles/pece/themes/pece_radix/
   bundle install
   bundle exec compass compile
 fi
