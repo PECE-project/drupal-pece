5a95b44b41bfb3507e26e8d1b6dd9754eaadac65
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-09 14:54:39 -0300

    Simplified gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-/**
- * @file Main gulp file.
- */
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file in gulp/tasks. Any files in that directory get
+  automatically required below.
+  To add a new task, simply add a new task file that directory.
+  gulp/tasks/default.js specifies the default set of tasks to run
+  when you run `gulp`.
+*/
 
-var gulp = require('gulp')
-  , requireDir = require('require-dir');
+var requireDir = require('require-dir');
 
-// Load all tasks.
-requireDir('./gulp', {
-  recurse: true
-});
-
-// Register default.
-// gulp.task('default', ['build']);
+requireDir('./gulp', { recurse: true });
