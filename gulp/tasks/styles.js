a870447b06c2cd33e1cb244cbb88114a9c1bfefd
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-09 18:26:47 -0300

    Add sass import paths.,

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,16 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function () {
-  return gulp.src('src/themes/pece_scholarly_lite/assets/sass/screen.sass')
-    .pipe(sass.sync().on('error', sass.logError))
+  return gulp.src('src/themes/pece_scholarly_lite/assets/sass/sources/**/*')
+    .pipe(sass({
+      includePaths: [
+        'src/themes/pece_scholarly_lite/assets/sass/library',
+        'src/themes/pece_scholarly_lite/assets/sass/partials'
+      ]
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('src/themes/pece_scholarly_lite/assets/stylesheets'));
+    .pipe(gulp.dest('src/themes/pece_scholarly_lite/assets/css'));
 });
