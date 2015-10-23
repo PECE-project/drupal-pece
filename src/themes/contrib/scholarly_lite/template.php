9451b03305bb8e69247a1abb8f1f5bc1120978bf
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-23 14:05:45 -0200

    Minor.

--- src/themes/contrib/scholarly_lite/template.php
+++ src/themes/contrib/scholarly_lite/template.php
@@ -11,12 +11,12 @@
 function scholarly_lite_breadcrumb($variables){
 
 	$breadcrumb = $variables['breadcrumb'];
-	
+
 	if (!empty($breadcrumb)) {
 	$breadcrumb[] = drupal_get_title();
 	return '<div>' . implode(' <span class="breadcrumb-separator"></span>', $breadcrumb) . '</div>';
 	}
-	
+
 }
 
 /**
@@ -26,7 +26,7 @@ function scholarly_lite_preprocess_block(&$variables) {
 
 	$variables['title_attributes_array']['class'][] = 'title';
 	$variables['classes_array'][]='clearfix';
-	
+
 }
 
 /**
@@ -37,9 +37,9 @@ function scholarly_lite_preprocess_html(&$variables) {
 	if (empty($variables['page']['banner'])) {
 		$variables['classes_array'][] = 'no-banner';
 	}
-	
+
 	$color_scheme = theme_get_setting('color_scheme');
-	
+
 	if ($color_scheme != 'default') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/style-' .$color_scheme. '.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
@@ -50,7 +50,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('paragraph_font_family')=='pff-1') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/merriweather-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-2'  ||
 		theme_get_setting('slogan_font_family')=='slff-2' ||
 		theme_get_setting('headings_font_family')=='hff-2' ||
@@ -64,7 +64,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('paragraph_font_family')=='pff-3') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/ubuntu-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-4'  ||
 		theme_get_setting('slogan_font_family')=='slff-4' ||
 		theme_get_setting('headings_font_family')=='hff-4' ||
@@ -85,42 +85,42 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('paragraph_font_family')=='pff-6') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/opensans-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-7'  ||
 		theme_get_setting('slogan_font_family')=='slff-7' ||
 		theme_get_setting('headings_font_family')=='hff-7' ||
 		theme_get_setting('paragraph_font_family')=='pff-7') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/lato-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-8'  ||
 		theme_get_setting('slogan_font_family')=='slff-8' ||
 		theme_get_setting('headings_font_family')=='hff-8' ||
 		theme_get_setting('paragraph_font_family')=='pff-8') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/roboto-condensed-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-9'  ||
 		theme_get_setting('slogan_font_family')=='slff-9' ||
 		theme_get_setting('headings_font_family')=='hff-9' ||
 		theme_get_setting('paragraph_font_family')=='pff-9') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/exo-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-10'  ||
 		theme_get_setting('slogan_font_family')=='slff-10' ||
 		theme_get_setting('headings_font_family')=='hff-10' ||
 		theme_get_setting('paragraph_font_family')=='pff-10') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/roboto-slab-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-11'  ||
 		theme_get_setting('slogan_font_family')=='slff-11' ||
 		theme_get_setting('headings_font_family')=='hff-11' ||
 		theme_get_setting('paragraph_font_family')=='pff-11') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/raleway-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-12'  ||
 		theme_get_setting('slogan_font_family')=='slff-12' ||
 		theme_get_setting('headings_font_family')=='hff-12' ||
@@ -134,21 +134,21 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('paragraph_font_family')=='pff-14') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/playfair-display-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-15'  ||
 		theme_get_setting('slogan_font_family')=='slff-15' ||
 		theme_get_setting('headings_font_family')=='hff-15' ||
 		theme_get_setting('paragraph_font_family')=='pff-15') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/philosopher-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-16'  ||
 		theme_get_setting('slogan_font_family')=='slff-16' ||
 		theme_get_setting('headings_font_family')=='hff-16' ||
 		theme_get_setting('paragraph_font_family')=='pff-16') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/cinzel-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-17'  ||
 		theme_get_setting('slogan_font_family')=='slff-17' ||
 		theme_get_setting('headings_font_family')=='hff-17' ||
@@ -162,14 +162,14 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('paragraph_font_family')=='pff-18') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/playfairdisplaysc-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-    
+
 	if (theme_get_setting('sitename_font_family')=='sff-19'  ||
 		theme_get_setting('slogan_font_family')=='slff-19' ||
 		theme_get_setting('headings_font_family')=='hff-19' ||
 		theme_get_setting('paragraph_font_family')=='pff-19') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/cabin-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	if (theme_get_setting('sitename_font_family')=='sff-20'  ||
 		theme_get_setting('slogan_font_family')=='slff-20' ||
 		theme_get_setting('headings_font_family')=='hff-20' ||
@@ -203,7 +203,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 		theme_get_setting('headings_font_family')=='hff-25' ||
 		theme_get_setting('paragraph_font_family')=='pff-25') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/alegreya-font.css', array('group' => CSS_THEME, 'type' => 'file'));
-	}	
+	}
 
 	if (theme_get_setting('sitename_font_family')=='sff-26'  ||
 		theme_get_setting('slogan_font_family')=='slff-26' ||
@@ -241,13 +241,13 @@ function scholarly_lite_preprocess_html(&$variables) {
 	}
 
 	drupal_add_css(path_to_theme() . '/fonts/sourcecodepro-font.css', array('group' => CSS_THEME, 'type' => 'file'));
-	
+
 	drupal_add_css(path_to_theme() . '/fonts/ptserif-blockquote-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 
     drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array('type' => 'external'));
 
 	drupal_add_css(path_to_theme() . '/ie9.css', array('group' => CSS_THEME, 'browsers' => array('IE' => '(IE 9)&(!IEMobile)', '!IE' => FALSE), 'preprocess' => FALSE));
-    
+
 	/**
 	 * Add local.css file for CSS overrides.
 	 */
@@ -260,33 +260,33 @@ function scholarly_lite_preprocess_html(&$variables) {
         $cdn = '//maxcdn.bootstrapcdn.com/bootstrap/' . theme_get_setting('bootstrap_css_cdn', 'scholarly_lite')  . '/css/bootstrap.min.css';
         drupal_add_css($cdn, array('type' => 'external'));
     }
-    
+
     if (theme_get_setting('bootstrap_js_cdn', 'scholarly_lite')) {
         $cdn = '//maxcdn.bootstrapcdn.com/bootstrap/' . theme_get_setting('bootstrap_js_cdn', 'scholarly_lite')  . '/js/bootstrap.min.js';
         drupal_add_js($cdn, array('type' => 'external'));
     }
-	
+
 	/**
 	 * Add Javascript for enable/disable scrollTop action.
 	 */
 	if (theme_get_setting('scrolltop_display')) {
-	
-		drupal_add_js('jQuery(document).ready(function($) { 
+
+		drupal_add_js('jQuery(document).ready(function($) {
 		$(window).scroll(function() {
 			if($(this).scrollTop() != 0) {
-				$("#toTop").addClass("show");	
+				$("#toTop").addClass("show");
 			} else {
 				$("#toTop").removeClass("show");
 			}
 		});
-		
+
 		$("#toTop").click(function() {
 			$("body,html").animate({scrollTop:0},800);
-		});	
-		
+		});
+
 		});',
 		array('type' => 'inline', 'scope' => 'header'));
-	
+
 	}
 	//EOF:Javascript
 
@@ -296,11 +296,11 @@ function scholarly_lite_preprocess_html(&$variables) {
 		/**
 		 * Add Javascript for the fixed header
 		 */
-		drupal_add_js('jQuery(document).ready(function($) { 
+		drupal_add_js('jQuery(document).ready(function($) {
 
 			var	headerTopHeight = $("#header-top").outerHeight(),
 			headerHeight = $("#header").outerHeight();
-			
+
 			$(window).scroll(function() {
 			if(($(this).scrollTop() > headerTopHeight+headerHeight) && ($(window).width() > 767)) {
 				$("body").addClass("onscroll");
@@ -308,7 +308,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 					$(".onscroll #logo").addClass("hide");
 				}
 
-				if ($("#banner").length > 0) { 
+				if ($("#banner").length > 0) {
  					$("#banner").css("marginTop", (headerHeight)+"px");
 				} else if ($("#page-intro").length > 0) {
 					$("#page-intro").css("marginTop", (headerHeight)+"px");
@@ -324,7 +324,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 		});',
 		array('type' => 'inline', 'scope' => 'header'));
 		//EOF:Javascript
-		
+
 	}
 
 	$responsive_meanmenu = theme_get_setting('responsive_multilevelmenu_state');
@@ -333,7 +333,7 @@ function scholarly_lite_preprocess_html(&$variables) {
 
 	drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/js/meanmenu/meanmenu.css');
 	drupal_add_js(drupal_get_path('theme', 'scholarly_lite') .'/js/meanmenu/jquery.meanmenu.fork.js', array('preprocess' => false));
-	
+
 		/**
 		 * Add Javascript for the mobile mean menu
 		 */
@@ -357,10 +357,10 @@ function scholarly_lite_preprocess_html(&$variables) {
 
 		});',
 		array('type' => 'inline', 'scope' => 'header'));
-		//EOF:Javascript	
+		//EOF:Javascript
 
-	} 
-    
+	}
+
 }
 
 /**
@@ -373,7 +373,7 @@ function scholarly_lite_process_html(&$vars) {
 	$classes[] = theme_get_setting('slogan_font_family');
 	$classes[] = theme_get_setting('headings_font_family');
 	$classes[] = theme_get_setting('paragraph_font_family');
-	$classes[] = theme_get_setting('form_style');	
+	$classes[] = theme_get_setting('form_style');
 	$vars['classes'] = trim(implode(' ', $classes));
 
 }
@@ -382,7 +382,7 @@ function scholarly_lite_process_html(&$vars) {
  * Preprocess variables for page template.
  */
 function scholarly_lite_preprocess_page(&$variables) {
-	
+
 	$three_columns_grid_layout = theme_get_setting('three_columns_grid_layout', 'scholarly_lite');
 	$sidebar_first = $variables['page']['sidebar_first'];
 	$sidebar_second = $variables['page']['sidebar_second'];
@@ -399,8 +399,8 @@ function scholarly_lite_preprocess_page(&$variables) {
 	/**
 	 * Insert variables into the page template.
 	 */
-	if (isset($variables['node']) && $variables['node']->type != 'page' ) { 
-		if($sidebar_first && $sidebar_second) { 
+	if (isset($variables['node']) && $variables['node']->type != 'page' ) {
+		if($sidebar_first && $sidebar_second) {
 			if ($three_columns_grid_layout == 'grid_3_6_3') {
 				$variables['main_grid_class'] = 'col-md-6';
 				$variables['sidebar_first_grid_class'] = 'col-md-3';
@@ -419,14 +419,14 @@ function scholarly_lite_preprocess_page(&$variables) {
 			$variables['sidebar_first_grid_class'] = 'col-md-4 fix-sidebar-first';
 		} elseif (!$sidebar_first && $sidebar_second) {
 			$variables['main_grid_class'] = 'col-md-8';
-			$variables['sidebar_second_grid_class'] = 'col-md-4 fix-sidebar-second';		
+			$variables['sidebar_second_grid_class'] = 'col-md-4 fix-sidebar-second';
 		} else {
 			$variables['main_grid_class'] = 'col-md-8 col-md-offset-2';
-			$variables['sidebar_first_grid_class'] = '';	
-			$variables['sidebar_second_grid_class'] = '';			
+			$variables['sidebar_first_grid_class'] = '';
+			$variables['sidebar_second_grid_class'] = '';
 		}
 	} else {
-		if($sidebar_first && $sidebar_second) { 
+		if($sidebar_first && $sidebar_second) {
 			if ($three_columns_grid_layout == 'grid_3_6_3') {
 				$variables['main_grid_class'] = 'col-md-6';
 				$variables['sidebar_first_grid_class'] = 'col-md-3';
@@ -445,42 +445,42 @@ function scholarly_lite_preprocess_page(&$variables) {
 			$variables['sidebar_first_grid_class'] = 'col-md-4 fix-sidebar-first';
 		} elseif (!$sidebar_first && $sidebar_second) {
 			$variables['main_grid_class'] = 'col-md-8';
-			$variables['sidebar_second_grid_class'] = 'col-md-4 fix-sidebar-second';		
+			$variables['sidebar_second_grid_class'] = 'col-md-4 fix-sidebar-second';
 		} else {
 			$variables['main_grid_class'] = 'col-md-12';
-			$variables['sidebar_first_grid_class'] = '';	
-			$variables['sidebar_second_grid_class'] = '';			
+			$variables['sidebar_first_grid_class'] = '';
+			$variables['sidebar_second_grid_class'] = '';
 		}
-	}	
+	}
 
-	if($header_top_left && $header_top_right) { 
+	if($header_top_left && $header_top_right) {
 		$variables['header_top_left_grid_class'] = 'col-md-8';
 		$variables['header_top_right_grid_class'] = 'col-md-4';
 	} elseif ($header_top_right || $header_top_left) {
 		$variables['header_top_left_grid_class'] = 'col-md-12';
-		$variables['header_top_right_grid_class'] = 'col-md-12';		
+		$variables['header_top_right_grid_class'] = 'col-md-12';
 	}
 
-	if($footer_top_left && $footer_top_right) { 
+	if($footer_top_left && $footer_top_right) {
 		$variables['footer_top_left_grid_class'] = 'col-sm-6';
 		$variables['footer_top_right_grid_class'] = 'col-sm-6';
 		$variables['footer_top_regions'] = 'two-regions';
 	} elseif ($footer_top_right || $footer_top_left) {
-		$variables['footer_top_regions'] = 'one-region';		
+		$variables['footer_top_regions'] = 'one-region';
 		$variables['footer_top_left_grid_class'] = 'col-md-12';
-		$variables['footer_top_right_grid_class'] = 'col-ms-12';		
+		$variables['footer_top_right_grid_class'] = 'col-ms-12';
 	}
 
-	if ($footer_first && $footer_second && $footer_third && $footer_fourth) { 
+	if ($footer_first && $footer_second && $footer_third && $footer_fourth) {
 		$variables['footer_grid_class'] = 'col-sm-3';
-	} elseif ((!$footer_first && $footer_second && $footer_third && $footer_fourth) || ($footer_first && !$footer_second && $footer_third && $footer_fourth) 
-	|| ($footer_first && $footer_second && !$footer_third && $footer_fourth) || ($footer_first && $footer_second && $footer_third && !$footer_fourth)) { 
+	} elseif ((!$footer_first && $footer_second && $footer_third && $footer_fourth) || ($footer_first && !$footer_second && $footer_third && $footer_fourth)
+	|| ($footer_first && $footer_second && !$footer_third && $footer_fourth) || ($footer_first && $footer_second && $footer_third && !$footer_fourth)) {
 		$variables['footer_grid_class'] = 'col-sm-4';
-	} elseif ((!$footer_first && !$footer_second && $footer_third && $footer_fourth) || (!$footer_first && $footer_second && !$footer_third && $footer_fourth) 
+	} elseif ((!$footer_first && !$footer_second && $footer_third && $footer_fourth) || (!$footer_first && $footer_second && !$footer_third && $footer_fourth)
 	|| (!$footer_first && $footer_second && $footer_third && !$footer_fourth) || ($footer_first && !$footer_second && !$footer_third && $footer_fourth)
 	|| ($footer_first && !$footer_second && $footer_third && !$footer_fourth) || ($footer_first && $footer_second && !$footer_third && !$footer_fourth)) {
-		$variables['footer_grid_class'] = 'col-sm-6';		
-	} else { 
+		$variables['footer_grid_class'] = 'col-sm-6';
+	} else {
 		$variables['footer_grid_class'] = 'col-sm-12';
 	}
 
@@ -495,7 +495,7 @@ function scholarly_lite_preprocess_maintenance_page(&$variables) {
 	if ($color_scheme != 'default') {
 		drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/style-' .$color_scheme. '.css', array('group' => CSS_THEME, 'type' => 'file'));
 	}
-	
+
 	drupal_add_css(drupal_get_path('theme', 'scholarly_lite') . '/fonts/lato-font.css', array('group' => CSS_THEME, 'type' => 'file'));
 
 }
@@ -529,11 +529,11 @@ function scholarly_lite_page_alter($page) {
 	drupal_add_html_head($mobileoptimized, 'MobileOptimized');
 	drupal_add_html_head($handheldfriendly, 'HandheldFriendly');
 	drupal_add_html_head($viewport, 'viewport');
-	
+
 }
 
 function scholarly_lite_form_alter(&$form, &$form_state, $form_id) {
-	
+
 	if ($form_id == 'search_block_form') {
 	unset($form['search_block_form']['#title']);
 	$form['search_block_form']['#title_display'] = 'invisible';
@@ -550,5 +550,3 @@ function scholarly_lite_form_alter(&$form, &$form_state, $form_id) {
 	}
 
 }
-
-?>
