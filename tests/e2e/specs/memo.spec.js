a775911dfa4461285a5eb1f677bdc4009cef7508
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-26 15:01:33 -0200

    Simplified memo tests by using seeds.

--- tests/e2e/specs/memo.spec.js
+++ tests/e2e/specs/memo.spec.js
@@ -1,66 +1,60 @@
 
-var simpleUser = {
-    username: 'simpleUser',
-    email: 'simpleUser@example.com',
-    pass: browser.params.admin.password,
-    name: 'SimpleUser',
-    tos: true
-}
+var Seeds = require('drupal-seeds').Seeds;
 
 describe('Memo', function () {
-  beforeEach(function() {
-    AuthenticationPage.logout();
-    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
-  });
+  var seeds = new Seeds([
+    {
+      type: 'user',
+      data: { name: 'contributor', mail: 'contributor@email.com', roles: ['Contributor'], pass: '123123' },
+      config: { accept_legal_terms: true }
+    },
+    {
+      type: 'user',
+      data: { name: 'contributor 2', mail: 'contributor2@email.com', roles: ['Contributor'], pass: '123123' },
+      config: { accept_legal_terms: true }
+    }
+  ]);
 
-  afterAll(function() {
-    AuthenticationPage.logout();
-    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
-    PeoplePage.deleteUser(RegistrationPage.defaultUser.email);
-  });
+  seeds.attach();
 
   it('Verify main elements presence', function() {
-   MemoPage.get();
-   MemoPage.checkMainElementsPresence();
+    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
+    MemoPage.get();
+    MemoPage.checkMainElementsPresence();
   });
 
   it('Verify mandatory fields', function() {
+    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
     MemoPage.get();
     MemoPage.checkMandatoryFields();
   });
 
   it('Add a Memo as an authenticated user', function() {
+    // @TODO: create a specific seed for this.
+    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
     SamplePage.get('admin/config/people/legal');
     LegalPage.create();
-    AuthenticationPage.logout();
-    RegistrationPage.registerProfile();
-    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
-    PeoplePage.unblock(RegistrationPage.defaultUser.email);
-    PeoplePage.addRole(RegistrationPage.defaultUser.email, 5);
-    AuthenticationPage.logout();
-    AuthenticationPage.login(RegistrationPage.defaultUser.username, RegistrationPage.defaultUser.pass);
+
+    AuthenticationPage.login('contributor', '123123');
     MemoPage.add('Memo', 'Lorem ipsum dolar sit.');
+
     MemoPage.checkPageLayout();
     MemoPage.checkPageElements();
   });
 
   it('should have comments opened but moderated for an authenticated users.', function() {
+    // @TODO: create a specific seed for this.
+    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
     SamplePage.get('admin/config/people/legal');
     LegalPage.create();
-    AuthenticationPage.logout();
-    RegistrationPage.registerProfile();
-    RegistrationPage.registerProfile(simpleUser);
-    AuthenticationPage.login(browser.params.admin.user, browser.params.admin.password);
-    PeoplePage.unblock(RegistrationPage.defaultUser.email);
-    PeoplePage.addRole(RegistrationPage.defaultUser.email, 5);
-    PeoplePage.unblock(simpleUser.email);
-    PeoplePage.addRole(simpleUser.email, 5);
-    AuthenticationPage.logout();
-    AuthenticationPage.login(RegistrationPage.defaultUser.username, RegistrationPage.defaultUser.pass);
+
+    // @TODO: replace with a node seed, created with user/author using 'seeds.values[0].uid'.
+    AuthenticationPage.login('contributor', 123123);
     MemoPage.add('Memo 2', 'Lorem ipsum dolar sit.');
-    AuthenticationPage.logout();
-    AuthenticationPage.login(simpleUser.username, simpleUser.pass);
+
+    AuthenticationPage.login('Contributor 2', 123123);
     MemoPage.comment('content/memo-2', 'Comment content');
+
     SamplePage.checkMessage('Your comment has been queued for review by site administrators and will be published after approval.');
   });
 });
