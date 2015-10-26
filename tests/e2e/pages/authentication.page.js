63db2ee01fe7528500f994c54274ba7656519ceb
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-26 14:59:51 -0200

    Simple authentication page improvement to avoid dumb logins/logouts.

--- tests/e2e/pages/authentication.page.js
+++ tests/e2e/pages/authentication.page.js
@@ -6,6 +6,8 @@ var EC = protractor.ExpectedConditions;
 
 var AuthenticationPage = function() {
 
+  var currentUser = null;
+
   // Define authentication attributes.
   this.usernameField = $('input#edit-name');
   this.passwordField = $('input#edit-pass');
@@ -13,16 +15,23 @@ var AuthenticationPage = function() {
   this.loggedIn      = $('body.logged-in');
 
   this.login = function (user, password) {
+    if (currentUser == user) return;
+    if (currentUser) this.logout();
+
     this.get();
     browser.wait(EC.visibilityOf(this.usernameField), browser.params.timeoutLimit);
     this.usernameField.sendKeys(user);
 
     this.passwordField.sendKeys(password);
     this.loginButton.click();
+
+    currentUser = user;
   };
 
   this.logout = function() {
+    if (!currentUser) return;
     browser.get('user/logout');
+    currentUser = null;
   }
 
   this.get = function() {
