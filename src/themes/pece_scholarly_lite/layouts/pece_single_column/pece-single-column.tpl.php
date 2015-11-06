fe8fcfb4ff5fe1e050d8e8cbc574418b49fd3731
Joseph Ramos <joseph.ramos@taller.net.br>
2015-11-06 14:56:55 -0500

    Created pece_single_column layout and updated homepage styles.

---
+++ src/themes/pece_scholarly_lite/layouts/pece_single_column/pece-single-column.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Template for Pece Single Column.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+
+<div class="panel-display sigle-column clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+
+  <div class="pece-layouts-content panel-panel">
+    <div class="panel-panel-inner">
+      <?php print $content['contentmain']; ?>
+    </div>
+  </div>
+
+</div><!-- /.boxton -->
