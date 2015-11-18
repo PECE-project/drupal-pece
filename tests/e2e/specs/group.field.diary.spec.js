9158d15e2140d608bb9571c8f53bdd7e8d9b1fed
wlsf82 <wlsf82@gmail.com>
2015-11-18 17:04:34 -0200

    Added no results test for group field diary page.

--- tests/e2e/specs/group.field.diary.spec.js
+++ tests/e2e/specs/group.field.diary.spec.js
@@ -2,23 +2,33 @@
 var Seeds = require('drupal-seeds').Seeds;
 
 describe('Group field diary', function () {
-  var seeds = new Seeds([
-    {
-      // type: 'user',
-      // data: { name: 'contributor', mail: 'contributor@email.com', roles: ['Contributor'], pass: '123123' },
-      // config: { accept_legal_terms: true }
+  var seeds = new Seeds([{
+    type: 'node',
+    data: {
+      type: 'pece_group',
+      title: 'Test',
     },
-    {
-      // type: 'user',
-      // data: { name: 'contributor 2', mail: 'contributor2@email.com', roles: ['Contributor'], pass: '123123' },
-      // config: { accept_legal_terms: true }
-    }
-  ]);
+    // {
+    //   type: 'user',
+    //   data: { name: 'contributor', mail: 'contributor@email.com', roles: ['contributor'], pass: '123123' },
+    //   config: { accept_legal_terms: true }
+    // },
+    // {
+    //   type: 'user',
+    //   data: { name: 'researcher', mail: 'researcher@email.com', roles: ['researcher'], pass: '123123' },
+    //   config: { accept_legal_terms: true }
+    // },
+  }]);
 
   seeds.attach();
 
   beforeAll(function() {
     AuthenticationPage.logout();
+  });
+
+  it('no results', function() {
+    FieldDiaryPage.get('pece-group-field-diary/' + seeds.values[0].nid);
+    expect(FieldDiaryPage.emptyResult.getText()).toEqual('There are no field notes for the current group.');
   });
 
   xit('verify main elements presence', function() {
@@ -34,10 +44,6 @@ describe('Group field diary', function () {
   }).pend('Test not created yet. TODO!');
 
   xit('private fieldnotes are only displayed for the fieldnote\'s author', function() {
-
-  }).pend('Test not created yet. TODO!');
-
-  xit('no results', function() {
 
   }).pend('Test not created yet. TODO!');
 });
