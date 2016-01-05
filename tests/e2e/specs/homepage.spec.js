1d47e6b0ac5fea3af3404aec65b39aa533ed7228
wlsf82 <wlsf82@gmail.com>
2016-01-05 14:41:56 -0200

    Updated home page tests to wait for elements before expecting things.

--- tests/e2e/specs/homepage.spec.js
+++ tests/e2e/specs/homepage.spec.js
@@ -22,16 +22,19 @@ describe('Homepage', function () {
 
   it('Should have the Group section', function() {
     HomePage.get();
+    browser.wait(HomePage.mainElements.groupsSection.isDisplayed);
     expect(HomePage.mainElements.groupsSection.getText()).toBe(sampleTitles[0])
   });
 
   it('Should have the Artifact section', function() {
     HomePage.get();
+    browser.wait(HomePage.mainElements.artifactsSection.isDisplayed);
     expect(HomePage.mainElements.artifactsSection.getText()).toBe(sampleTitles[1])
   });
 
   it('Should have the Tags section', function() {
     HomePage.get();
+    browser.wait(HomePage.mainElements.tagsSection.isDisplayed);
     expect(HomePage.mainElements.tagsSection.getText()).toBe(sampleTitles[2])
   });
 
