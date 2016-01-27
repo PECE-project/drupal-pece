b78ed07dd528216f6b3927ce63e2fcc6ae709366
wlsf82 <wlsf82@gmail.com>
2016-01-27 20:15:48 -0200

    Added structure for smoke test.

---
+++ tests/e2e/smoke.conf.js
@@ -0,0 +1,70 @@
+/**
+* @file conf.js
+*/
+
+var fs = require('fs');
+var SeedsAPI = require('drupal-seeds');
+var SpecReporter = require('jasmine-spec-reporter');
+
+module.exports.config = {
+  params: {
+    admin: {
+      user: 'admin',
+      password: 'impossiblepassword'
+    }
+  },
+
+  specs: ['smoke.spec.js'],
+  baseUrl: 'http://pece.local/',
+
+  capabilities: { 'browserName': 'chrome' },
+  framework: 'jasmine2',
+  directConnect: true,
+
+  // Here you will set things that have to happen before start testing.
+  onPrepare: function() {
+    // Load all page objects to the global scope.
+    require('./pages/global');
+
+    // Used for non-angular apps
+    browser.ignoreSynchronization = true;
+
+    // Add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayFailuresSummary: true,
+      displayFailedSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }));
+
+    browser.driver.manage().window().maximize();
+
+    // Used to define a default delay between actions.
+    var origFn = browser.driver.controlFlow().execute;
+
+    browser.driver.controlFlow().execute = function() {
+      var args = arguments;
+
+      // queue 200ms wait.
+      origFn.call(browser.driver.controlFlow(), function() {
+        return protractor.promise.delayed(100);
+      });
+
+      return origFn.apply(browser.driver.controlFlow(), args);
+    };
+
+    // Configure Seeds API.
+    return SeedsAPI.initProtractor();
+  },
+
+  jasmineNodeOpts: {
+    showColors: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 999999
+  }
+};
+
+// Allow for local config customization.
+if (fs.existsSync(__dirname + '/config-alter.js')) {
+  require(__dirname + '/config-alter.js')(module.exports.config);
+}
---
+++ tests/e2e/smoke.spec.js
@@ -0,0 +1,58 @@
+var Seeds = require('drupal-seeds').Seeds;
+
+describe('Smoke test', function() {
+
+  var seeds = new Seeds([
+    {
+      type: 'user',
+      data: {
+        name: 'smoke-test-user',
+        mail: 'smoke@test-user.com',
+        pass: '123123',
+        roles: [
+          'contributor',
+          'researcher'
+        ]
+      }
+    },
+    {
+      type: 'term',
+      data: {
+        type: 'pece_tags',
+        name: 'smoke-test-tag'
+      }
+    },
+    {
+      type: 'node',
+      data: {
+        type: 'pece_artifact_text',
+        title: 'Smoke test artifact text',
+        field_pece_tags: [Seeds.parser(1, 'tid')]
+      }
+    },
+    {
+      type: 'node',
+      data: {
+        type: 'pece_memo',
+        title: 'Smoke test memo'
+      }
+    },
+    {
+      type: 'node',
+      data: {
+        type: 'pece_group',
+        title: 'Smoke test group'
+      }
+    }
+  ]);
+
+  seeds.attach();
+
+  it('navigate throught the site as a anonymous user', function() {
+    SamplePage.get();
+    browser.pause();
+  });
+
+});
+
+// require('./specs/some-test.spec.js');
