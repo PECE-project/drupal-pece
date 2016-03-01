ffbb81acfe3defd56e65948e3f94c403f0911497
Mario <mario@taller.net.br>
2016-03-01 17:47:55 -0300

    Updated frontpage slideshow with custom js.

---
+++ src/modules/pece/pece_frontpage_slideshow/js/pece_frontpage_slideshow.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+  $('.view-id-frontpage_image_slider').cycle({
+    fx: 'scrollLeft',
+    fit: 0,
+    width: '100%',
+    slideExpr: '.view-content div img'
+  });
+
+})(jQuery);
