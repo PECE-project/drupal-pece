5abe5368de9a3f822567564521717ac2dae0f0c6
lhguerra <lhguerra@gmail.com>
2016-04-14 16:31:26 -0300

    Added initial functionality for artifacts to use fieldsite's location.

---
+++ src/modules/pece/pece_artifacts/pece_artifacts.api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Hooks provided by the PECE Artifacts module.
+ */
+
+/**
+ * Define artifact types.
+ *
+ * @return $artifacts.
+ */
+function hook_pece_artifact_info() {
+}
+
+/**
+ * Alter the registry of artifact types.
+ *
+ * @param $artifacts.
+ */
+function hook_pece_artifact_info_alter(&$artifacts) {
+}
