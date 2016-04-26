66dbbd16b3d74f58261a6417452fd37356dcbb2c
Renato Vasconcellos <revagomes@gmail.com>
2016-04-26 16:37:19 -0300

    Fixed jquery cycle issue in frontpage slideshow.

--- src/modules/pece/pece_frontpage_slideshow/js/pece_frontpage_slideshow.js
+++ src/modules/pece/pece_frontpage_slideshow/js/pece_frontpage_slideshow.js
@@ -1,10 +1,12 @@
 (function ($) {
 
-  $('.view-id-frontpage_image_slider').cycle({
-    fx: 'scrollLeft',
-    fit: 0,
-    width: '100%',
-    slideExpr: '.view-content div img'
+  $(document).ready(function () {
+    $('.view-id-frontpage_image_slider').cycle({
+      fx: 'scrollLeft',
+      fit: 0,
+      width: '100%',
+      slideExpr: '.view-content div img'
+    });
   });
 
 })(jQuery);
