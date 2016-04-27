cd70f3aaa498ec69969ee57e900a537915197881
Renato Vasconcellos <revagomes@gmail.com>
2016-04-27 18:10:34 -0300

    Fixed gulp tasks for build and update the distro.

--- gulp/tasks/update.js
+++ gulp/tasks/update.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 var sequence = require('gulp-sequence');
 
 gulp.task('update', function (done) {
-  sequence(['bower:install'], 'styles', done);
+  sequence(['drush:kw-u', 'bower:install'], 'styles', done);
 });
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
 
-drush kw-b
-
-(
-  cd ./build
-)
-drush kw-u
-## Uncomment for CM left sync.
-# drush clsyn
-
-# Compile theme stylesheets.
+# Build and Compile theme stylesheets.
 (
   cd $ROOT_DIR
+  node_modules/.bin/gulp build
 )
-node_modules/.bin/gulp styles
 
 sh ../../scripts/sample_content.sh
