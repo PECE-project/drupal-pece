fae07098dcb0d2b49d9a90fb625fbbe589eac2ca
Renato Vasconcellos <revagomes@gmail.com>
2016-04-27 18:10:34 -0300

    Fixed gulp tasks for build and update the distro.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
-
-(
-  cd ./build
-)
-drush kw-u
-
-# Compile theme stylesheets.
+# Update and Compile theme stylesheets.
 (
   cd $ROOT_DIR
+  node_modules/.bin/gulp update
 )
-node_modules/.bin/gulp styles
