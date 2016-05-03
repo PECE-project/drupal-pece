242edd53dcdf03cbcc570f3f0187260bcfd84965
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-05-03 15:24:24 -0300

    Simplified build gulp process and attached it to npm install.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -5,8 +5,8 @@ var sequence = require('gulp-sequence');
 var gulp     = require('gulp');
 var profile  = 'profiles/pece';
 
-gulp.task('build', ['styles'], function (done) {
-  sequence('drush:kw-b', ['drush:kw-u', 'update'], done);
+gulp.task('build', function (done) {
+  sequence('bower:install', 'styles', 'drush:kw-b', done);
 });
 
 gulp.task('build:dev', ['drush:kw-b'], function (done) {
@@ -14,6 +14,5 @@ gulp.task('build:dev', ['drush:kw-b'], function (done) {
   cd(cwd + '/build');
   exec('drush make ' + profile + '/pece.dev.make --no-core --contrib-destination ' + profile + ' -y');
   cd(cwd);
-
-  sequence(['drush:kw-u', 'update'], done);
+  done();
 });
