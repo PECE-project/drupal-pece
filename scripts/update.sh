a32a5f23e78746070a03db82992ab957e527f66e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-05-08 00:12:56 -0300

    Removed bash scripts.

--- scripts/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
-# Update and Compile theme stylesheets.
-(
-  cd $ROOT_DIR
-  node_modules/.bin/gulp update
-)
