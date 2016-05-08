a59d0e2f44d5f267589e4ee084e98def55d2262e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-05-07 23:35:22 -0300

    Added gulp command to install PECE database.

---
+++ gulp/tasks/drush/si.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var shell = require('shelljs');
+
+var cwd = process.cwd(), command;
+var password = process.env.PECE_DRUPAL_ADMIN_PASS || 'impossiblepassword';
+var siteName = process.env.PECE_DRUPAL_SITE_NAME || 'PECE Drupal Distro';
+
+gulp.task('drush:si', function () {
+  command = '';
+  command += 'drush si pece';
+  command += ' --site-name="' + siteName + '"';
+  command += ' --account-pass="' + password + '"';
+  command += ' -y';
+
+  shell.cd(cwd + '/build');
+  shell.exec(command);
+  shell.cd(cwd);
+});
