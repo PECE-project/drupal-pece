6faaabbbfaf054c29eecef32e6b6a5343f716a34
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-05-11 21:04:32 -0300

    Added error filter for gulp site-install task.

--- gulp/tasks/drush/si.js
+++ gulp/tasks/drush/si.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var shell = require('shelljs');
 
-var cwd = process.cwd(), command;
+var cwd = process.cwd(), command, failed, message;
 var password = process.env.PECE_DRUPAL_ADMIN_PASS || 'impossiblepassword';
 var siteName = process.env.PECE_DRUPAL_SITE_NAME || 'PECE Drupal Distro';
+var acceptableErrors = [
+  'Couldn\'t write .htaccess file',
+  'notify-send command failed'
+];
 
-gulp.task('drush:si', function () {
+gulp.task('drush:si', function (done) {
+  shell.cd(cwd + '/build');
+
   command = '';
   command += 'drush si pece';
   command += ' --site-name="' + siteName + '"';
@@ -14,7 +20,20 @@ gulp.task('drush:si', function () {
   command += ' --verbose';
   command += ' -y';
 
-  shell.cd(cwd + '/build');
-  shell.exec(command);
-  shell.cd(cwd);
+  failed = false;
+  shell.exec(command, end).stderr.on('data', onError);
+
+  function end(code) {
+    shell.cd(cwd);
+    done(failed ? code : 0);
+  }
+
+  function onError(data) {
+    if (!failed) {
+      var message = data.toString();
+      failed = !acceptableErrors.some(function (pattern) {
+        return message.indexOf(pattern) > -1;
+      });
+    }
+  }
 });
