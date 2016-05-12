be473aa58f357fe5449f731289df69cc948eb938
Luiz Amorim <luiz.simples@gmail.com>
2016-05-12 17:21:27 -0300

    Adjust README.md

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,11 +4,11 @@ while ! nc -q 1 pece-db 3306 </dev/null; do sleep 3; done
 
 sudo chown -R www-data:www-data /pece/drupal/cnf/files
 sudo service php5-fpm restart
-sudo nginx -g "daemon on;"
+sudo nginx -g "daemon on;" > /tmp/access-nginx.log
 
 if [ ! -d /pece/drupal/node_modules ];
 then
-  npm install && gulp setup
+  npm install && gulp setup && gulp build
 fi
 
 echo ""
