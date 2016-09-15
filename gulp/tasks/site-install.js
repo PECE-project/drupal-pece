82fc2f95c5b1bc21087b01dcfbe0afeada8e0fbe
Luiz Guerra <lhguerra@gmail.com>
2016-09-15 14:25:30 -0300

    Updated site-install task to avoid installing when running make distro.

--- gulp/tasks/site-install.js
+++ gulp/tasks/site-install.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sequence = require('gulp-sequence');
+var shell = require('shelljs');
 
 gulp.task('site-install', function (done) {
+  // Avoid site-install if we are just running a build for the distro.
+  if (process.env.IS_PRODUCTION) {
+    return done();
+  }
   sequence('drush:si', 'drush:kw-apply-module-dependencies', 'drush:cc', done)
 });
