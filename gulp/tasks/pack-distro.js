4a2e17fcdbbfc6509de8d4072f7a189040e4816b
Renato Vasconcellos <revagomes@gmail.com>
2017-01-13 11:35:46 -0200

    Update gulp pack-distro task to build as production env.

--- gulp/tasks/pack-distro.js
+++ gulp/tasks/pack-distro.js
@@ -1,55 +1,40 @@
 var gulp = require('gulp');
 var shell = require('shelljs');
-var sequence = require('gulp-sequence');
 var fs = require('fs');
 
 var cwd = process.cwd();
+var kwEnvConfigFile = cwd + '/cnf/environment';
 var repo = 'https://github.com/PECE-project/pece-distro.git';
-// var repo = 'https://github.com/revagomes/pece-distro.git';
 var commitMessage = 'Added new release based on master of development repository.'
 
-gulp.task('pack-distro', function (done) {
+gulp.task('pack-distro', function () {
   // Clone distro repository.
   fs.stat('distro', function (err) {
     if (err == null) {
       shell.exec('rm -rf distro');
     }
-    packDistroCallback(done);
+    packDistroCallback();
   });
 
 });
 
-var packDistroCallback = function (done) {
+var packDistroCallback = function () {
+  // Backup kw previous config and set kraftwagen env config to production
+  // in order to prevent symlinks in the profile filder.
+  shell.exec('mv ' + kwEnvConfigFile + ' ' + kwEnvConfigFile + '.bkp');
   shell.exec('drush kw-setup-env production');
-  sequence('build', function (cb) {
-    shell.exec('drush kw-setup-env development');
-    shell.exec('git clone ' + repo + ' distro');
+  shell.exec('drush kw-b');
+  // Remove the updated config file and restore previous kraftwagen env config.
+  shell.exec('rm ' + kwEnvConfigFile);
+  shell.exec('mv ' + kwEnvConfigFile + '.bkp ' + kwEnvConfigFile);
 
-    // Update distro repository with a new build.
-    shell.exec('rsync -rkptgozhD --delete --progress --exclude=profiles/pece/docs  build/* distro/');
-    shell.cd('distro');
+  shell.exec('git clone ' + repo + ' distro');
 
-  // shell.exec('rm profiles/pece/.gitignore');
+  // Update distro repository with a new build.
+  shell.exec('rsync -rkptgozhD --delete --progress --exclude=profiles/pece/docs  build/* distro/');
+  shell.cd('distro');
+  shell.exec('git add -A');
 
-  //   var gitIgnoreDistro = `# ignore modules in kraftwagen, but keep the README.md
-  //   modules/dev/*
-  //   !modules/dev/README.md
-  //
-  //   # OS specific files
-  //   .DS_Store
-  //   Thumbs.db
-  // `;
-    // shell.exec('echo ' + gitIgnoreDistro + ' >> .gitignore');
-
-    shell.exec('git add -A');
-
-
-    // shell.exec('git add profiles/pece -f');
-    // shell.exec('git reset profiles/pece/docs');
-    // shell.exec('git reset profiles/pece/modules/dev');
-
-    shell.exec(`git commit -m "${ commitMessage }"`);
-    // shell.exec(`git push ${repo} master`);
-    cb();
-  }, done);
+  shell.exec('git commit -m "' + commitMessage + '"');
+  shell.exec('git push ' + repo + ' master');
 }
