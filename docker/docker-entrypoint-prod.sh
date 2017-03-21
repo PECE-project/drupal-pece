7646a4c3206bb75aa0b43d973ad5e3db38b30edf
Renato Vasconcellos <revagomes@gmail.com>
2017-03-21 17:37:00 -0300

    Updted docker-compose file to add a new entrypoint for the production service settings.

---
+++ docker/docker-entrypoint-prod.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo chown $UID:$UID -R .
+sudo chmod +w -R .
+while ! nc -q 1 pece-db 3306 </dev/null; do sleep 3; done
+
+sudo service php5-fpm restart
+sudo nginx -g "daemon on;" > /tmp/access-nginx.log
+
+echo ""
+echo "--------------------------------"
+echo "------ Database connected ------"
+echo "--------------------------------"
+echo ""
+
+echo ""
+echo "-------------------------------"
+echo "-- Started the build process --"
+echo "-------------------------------"
+echo ""
+
+drush kw-b
+
+echo "PECE's Build proccess succeeded."
+
+echo ""
+echo "------------------------------"
+echo "Virtual machine ready to work."
+echo "------------------------------"
+echo ""
+
+exec "$@"
