003ba1175dfb33a71a58b857b417873a2a92ab2f
Renato Vasconcellos <revagomes@gmail.com>
2017-04-25 20:02:21 -0300

    Fixed Travis CI before_install config.

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -4,9 +4,6 @@ set -ev
 
 ls -lah
 
-nvm use default
-gulp update
-
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
   ./build ubuntu@138.197.1.62:/home/ubuntu/drupal-pece/build
 
