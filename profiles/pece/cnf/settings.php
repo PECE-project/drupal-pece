5d952b9997ec5ece081892dd5c805f5391361047
Renato Vasconcellos <revagomes@gmail.com>
2017-06-30 14:49:50 -0300

    Enabled user register email notifications by default.

--- profiles/pece/cnf/settings.php
+++ profiles/pece/cnf/settings.php
@@ -8,8 +8,3 @@ ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 100);
 ini_set('session.gc_maxlifetime', 200000);
 ini_set('session.cookie_lifetime', 2000000);
-
-# @TODO: Restore email notifications in the future.
-$conf['user_mail_register_pending_approval_notify'] = FALSE;
-$conf['user_mail_register_no_approval_required_notify'] = FALSE;
-
