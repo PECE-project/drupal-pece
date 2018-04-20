a8e50640db74e446dadbeaf44023516f51879df6
Renato Vasconcellos <revagomes@gmail.com>
2018-04-20 15:13:34 -0300

    Fix PHP7 compatibility issues on Biblio module.

--- profiles/pece/modules/contrib/biblio/modules/bibtexParse/PARSEMONTH.php
+++ profiles/pece/modules/contrib/biblio/modules/bibtexParse/PARSEMONTH.php
@@ -5,7 +5,7 @@ Do whatever you like with this -- some credit to the author(s) would be apprecia
 
 A collection of PHP classes to manipulate bibtex files.
 
-If you make improvements, please consider contacting the administrators at bibliophile.sourceforge.net so 
+If you make improvements, please consider contacting the administrators at bibliophile.sourceforge.net so
 that your improvements can be added to the release package.
 
 Mark Grimshaw 2005
@@ -26,13 +26,13 @@ http://bibliophile.sourceforge.net
 class PARSEMONTH
 {
 // Constructor
-  function PARSEMONTH()
+  function __construct()
   {
   }
   function init($monthField)
   {
     $startMonth = $this->startDay = $endMonth = $this->endDay = FALSE;
-    $date = split("#", $monthField);
+    $date = preg_split("#", $monthField);
     foreach ($date as $field)
     {
       $field = ucfirst(strtolower(trim($field)));
