8aa42e93e7b4b11d614d42bed1ca29f4e3094578
Renato Vasconcellos <revagomes@gmail.com>
2018-10-15 13:52:07 -0300

    Move timeline-pane template file.

--- src/modules/pece/pece_timeline_essay/templates/timeline-pane.tpl.php
+++ src/modules/pece/pece_timeline_essay/templates/timeline-pane.tpl.php
@@ -1,13 +1,13 @@
 <div class="pece-timeline-pane">
   <h1>TIMELINE PANE TEMPLATE</h1>
-  <?php if (!empty($content['description'])) : ?>
+  <?php if (!empty($content['timeline'])) : ?>
 
     <div class="<?php print $content['class']; ?>">
       <?php if (!empty($content['title'])) : ?>
         <h2><?php print $content['title']; ?></h2>
       <?php endif; ?>
 
-      <p><?php print render($content['description']); ?></p>
+      <p><?php print render($content['timeline']); ?></p>
     </div>
 
   <?php endif; ?>
