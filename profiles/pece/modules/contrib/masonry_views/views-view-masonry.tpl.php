c3b603a0b2814c87c65d41634aca4fbfd440cc3d
Renato Vasconcellos <revagomes@gmail.com>
2019-07-05 18:26:37 +0000

    Upgrade Masonry Views module version to v3.0

--- profiles/pece/modules/contrib/masonry_views/views-view-masonry.tpl.php
+++ profiles/pece/modules/contrib/masonry_views/views-view-masonry.tpl.php
@@ -5,9 +5,17 @@
  */
 ?>
 
+<?php if (isset($grouping) && $grouping): ?>
+  <?php if (!empty($title)): ?>
+    <h3 style="clear:both;"><?php print $title; ?></h3>
+    <?php endif; ?>
+  <?php print $prefix ?>
+<?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
   <div class="masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
-
+<?php if(isset($grouping) && $grouping): ?>
+  <?php print $suffix ?>
+<?php endif;?>
