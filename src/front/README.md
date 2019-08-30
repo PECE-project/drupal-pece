8118b88faa9d11f566aa696af1c83603f1c2f65a
Alan Ktquez <ktquez@gmail.com>
2019-08-30 16:53:18 -0300

    Add frontend structure in src/front

---
+++ src/front/README.md
@@ -0,0 +1,25 @@
+# frontend-v2-pece
+
+> front-end for v2 PECE distribution
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# Copy environment file and update data
+$ cp .env.example .env
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ src/front/pages/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        V2 PECE
+      </h1>
+      <h2 class="subtitle">
+        {{ $t('description') }}
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.title, .subtitle {
+  color: $primary-color;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
