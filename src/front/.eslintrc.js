2f41b114823bd90fc7b7880fab975bf115d61d1d
Alan Ktquez <ktquez@gmail.com>
2019-12-17 17:30:40 -0300

    Add plugins and settings for code accessibility

--- src/front/.eslintrc.js
+++ src/front/.eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     browser: true,
     node: true
   },
+  plugins: [
+    'vue-a11y'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue-a11y/base'
   ],
   rules: {
     'no-console': 'off',
--- src/front/nuxt.config.js
+++ src/front/nuxt.config.js
@@ -26,7 +26,14 @@ module.exports = {
    ** Plugins to load before mounting the App
    */
   plugins: [
-    './plugins/composition-api'
+    // Doc: https://github.com/vuejs/composition-api
+    './plugins/composition-api',
+    // Doc: https://github.com/vue-a11y/vue-announcer
+    { src: './plugins/a11y/vue-announcer', mode: 'client' },
+    // Doc: https://github.com/vue-a11y/vue-skip-to
+    { src: './plugins/a11y/vue-skip-to', mode: 'client' },
+    // Doc: https://github.com/vue-a11y/vue-axe
+    { src: './plugins/a11y/vue-axe', mode: 'client' },
   ],
   /*
    ** Nuxt.js dev-modules
