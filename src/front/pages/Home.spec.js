85d3ccffd9822f1ed76c31970e5b862e7273297a
Alan Ktquez <ktquez@gmail.com>
2019-12-18 09:11:53 -0300

    Configure unit and e2e tests

---
+++ src/front/pages/Home.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/pages/Home.vue'
+
+describe('Home', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(Home, {
+      mocks: {
+        $t: key => key
+      }
+    })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
