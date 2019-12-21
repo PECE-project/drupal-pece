16000d676f25fba2d75392dbae66be1a6495a2ec
Alan Ktquez <ktquez@gmail.com>
2019-12-20 23:24:19 -0300

    Update in unit tests config

--- src/front/components/Logo.test.js
+++ src/front/components/Logo.test.js
@@ -1,9 +1,10 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
+
 import Logo from '@/components/Logo.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = shallowMount(Logo)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
--- src/front/jest.config.js
+++ src/front/jest.config.js
@@ -5,11 +5,17 @@ module.exports = {
     '^vue$': 'vue/dist/vue.common.js'
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
+  testMatch: [
+    '**/tests/unit/**/*.test.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+    '**/(*.)test.(js|jsx|ts|tsx)'
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
   },
+  coverageReporters: ['html', 'text-summary'],
   collectCoverage: true,
+  coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
