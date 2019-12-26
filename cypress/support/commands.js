ad568a36954e0a780f5084e19da8a9b605eb023e
Edmar Gomes <edmar.gomes@nyx.tc>
2019-12-26 18:38:02 -0300

    Add test access to cy commands.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("testNoAccess", (url) => {
+  cy.request({
+    url: url,
+    failOnStatusCode: false // turn off following redirects
+  })
+    .then((resp) => {
+      expect(resp.status).to.eq(403)
+    })
+});
+
+Cypress.Commands.add("testAccess", (url) => {
+  cy.request({
+    url: url
+  })
+    .then((resp) => {
+      expect(resp.status).to.eq(200)
+    })
+});
