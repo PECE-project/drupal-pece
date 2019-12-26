b3e37639b66dfb5e92ac08018c24adbc7cc6ddfb
Alan Ktquez <ktquez@gmail.com>
2019-12-26 08:20:14 -0300

    Fix: vue-axe only development

--- src/front/plugins/a11y/vue-axe.js
+++ src/front/plugins/a11y/vue-axe.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   const VueAxe = require('vue-axe')
   Vue.use(VueAxe, {
     config: {
