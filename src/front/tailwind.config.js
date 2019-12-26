dac69739d2eb061ac352ce64b8051d6cbc6164db
Alan Ktquez <ktquez@gmail.com>
2019-12-26 08:21:27 -0300

    Custom tailwind config and sass files

--- src/front/tailwind.config.js
+++ src/front/tailwind.config.js
@@ -5,7 +5,23 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
-  variants: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: '',
+        accent: '#6C6FD4',
+        accentHover: '#595BB5'
+      },
+      boxShadow: {
+        search: '0 6px 4px #F7FAFC'
+      }
+    }
+  },
+  variants: {
+    extends: {
+      borderWidth: ['responsive', 'hover', 'focus'],
+      width: ['responsive', 'hover', 'focus']
+    }
+  },
   plugins: []
 }
