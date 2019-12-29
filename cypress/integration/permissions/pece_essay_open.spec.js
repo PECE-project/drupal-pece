020595f596e97df888e376231afd1c7f0354c42a
Edmar Gomes <edmar.gomes@nyx.tc>
2019-12-29 19:04:15 -0300

    Complete tests to open content and private group.

--- cypress/integration/permissions/pece_essay_open.spec.js
+++ cypress/integration/permissions/pece_essay_open.spec.js
@@ -17,8 +17,8 @@ context('Permissions', () => {
 
   const title = "Open PECE Essay cy"
   const path = "/content/open-pece-essay-cy"
-  const groupName = 'Group Test cy'
-  const groupPath = '/content/group-test-cy'
+  const publicGroup = 'Public Group Test cy'
+  const privateGroup = 'Private Group Test cy'
 
   describe ('Create contents to tests', () => {
 
@@ -40,10 +40,21 @@ context('Permissions', () => {
       })
     })
 
-    it('create group', () => {
+    it('create public group', () => {
       cy.login('admin')
       cy.createContent('/node/add/pece-group', [
-        'input[name=title]:type:' + groupName
+        'input[name=title]:type:' + publicGroup
+      ], () => {
+        cy.addImage('#edit-field-pece-media-image-und-0-upload--widget')
+        cy.type_tinyMCE('edit-body-und-0-value', "<p>Test group</p>")
+      })
+    })
+
+    it('create private group', () => {
+      cy.login('admin')
+      cy.createContent('/node/add/pece-group', [
+        'input[name=title]:type:' + privateGroup,
+        '#edit-group-access-und-1:check:1'
       ], () => {
         cy.addImage('#edit-field-pece-media-image-und-0-upload--widget')
         cy.type_tinyMCE('edit-body-und-0-value', "<p>Test group</p>")
@@ -75,7 +86,7 @@ context('Permissions', () => {
         it ('group with default visibility', () => {
           cy.login('admin')
           cy.updateContent(title, [
-            '#edit-og-group-ref-und-0-default:select:' + groupName
+            '#edit-og-group-ref-und-0-default:select:' + publicGroup
           ])
         })
         it("anonymous user can access this content", () => {
@@ -87,7 +98,7 @@ context('Permissions', () => {
       describe('Public Visibility', () => {
         it("change group visibility TO PUBLIC", () => {
           cy.login('admin')
-          cy.updateContent(groupName, [
+          cy.updateContent(publicGroup, [
             '#edit-group-content-access-und:select:Public - accessible to all site users'
           ])
         })
@@ -100,7 +111,7 @@ context('Permissions', () => {
       describe('Private Visibility', () => {
       it ("change group visibility TO PRIVATE", () => {
         cy.login('admin')
-        cy.updateContent(groupName, [
+        cy.updateContent(publicGroup, [
           '#edit-group-content-access-und:select:Private - accessible only to group members'
         ])
       })
@@ -144,10 +155,10 @@ context('Permissions', () => {
       it ('add user and researcher in the content group', () => {
         cy.login('admin')
         cy.updateUser('user', [
-          '#edit-og-user-node-und-0-default:select:' + groupName
+          '#edit-og-user-node-und-0-default:select:' + publicGroup
         ])
         cy.updateUser('researcher', [
-          '#edit-og-user-node-und-0-default:select:' + groupName
+          '#edit-og-user-node-und-0-default:select:' + publicGroup
         ])
       })
 
@@ -166,13 +177,149 @@ context('Permissions', () => {
       //@TODO: Need test to add two groups, one public and visibility public other private and permission need keep public
     })
     })
-  })
 
+    describe('Private group', () => {
+
+      describe('Default Visibility', () => {
+        it ('group with default visibility', () => {
+          cy.login('admin')
+          cy.updateContent(title, [
+            '#edit-og-group-ref-und-0-default:select:' + privateGroup
+          ])
+        })
+        it("anonymous user can't access this content", () => {
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('authenticated user can\'t access this content', () => {
+          cy.login('user')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('researcher user can\'t access this content', () => {
+          cy.login('researcher')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('contributor user can access this content', () => {
+          cy.login('contributor')
+          cy.testAccess(path)
+          cy.testAccess(path + '/essay')
+        })
+
+        it('contributor user can access in panels editor', () => {
+          cy.login('contributor')
+          cy.visit(path + "/essay")
+          cy.contains('Customize this page').click()
+          cy.contains('Save as custom')
+          cy.contains('Cancel')
+        })
+
+        it("researcher user can't access this content", () => {
+          cy.login('researcher')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it ('add user in the content group', () => {
+          cy.login('admin')
+          cy.updateUser('user', [
+            '#edit-og-user-node-und-0-default:select:' + privateGroup
+          ])
+        })
+
+        it("user in group content can access this content", () => {
+          cy.login('user')
+          cy.testAccess(path)
+          cy.testAccess(path + '/essay')
+        })
+
+      })
+
+      describe('Public Visibility', () => {
+        it("change group visibility TO PUBLIC", () => {
+          cy.login('admin')
+          cy.updateContent(publicGroup, [
+            '#edit-group-content-access-und:select:Public - accessible to all site users'
+          ])
+        })
+        it("anonymous user can access this content", () => {
+          cy.testAccess(path)
+          cy.testAccess(path + '/essay')
+        })
+      })
+
+      describe('Private Visibility', () => {
+        it ("change group visibility TO PRIVATE", () => {
+          cy.login('admin')
+          cy.updateContent(privateGroup, [
+            '#edit-group-content-access-und:select:Private - accessible only to group members'
+          ])
+        })
+        it("anonymous user can't access this content", () => {
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('authenticated user can\'t access this content', () => {
+          cy.login('user')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('researcher user can\'t access this content', () => {
+          cy.login('researcher')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it('contributor user can access this content', () => {
+          cy.login('contributor')
+          cy.testAccess(path)
+          cy.testAccess(path + '/essay')
+        })
+
+        it('contributor user can access in panels editor', () => {
+          cy.login('contributor')
+          cy.visit(path + "/essay")
+          cy.contains('Customize this page').click()
+          cy.contains('Save as custom')
+          cy.contains('Cancel')
+        })
+
+        it("researcher user can't access this content", () => {
+          cy.login('researcher')
+          cy.testNoAccess(path)
+          cy.testNoAccess(path + '/essay')
+        })
+
+        it ('add researcher in the content group', () => {
+          cy.login('admin')
+          cy.updateUser('researcher', [
+            '#edit-og-user-node-und-0-default:select:' + privateGroup
+          ])
+        })
+
+        it("researcher user in group user can access this content", () => {
+          cy.login('researcher')
+          cy.testAccess(path)
+          cy.testAccess(path + '/essay')
+        })
+
+        //@TODO: Need test to add two groups, one public and visibility public other private and permission need keep public
+      })
+    })
+  })
+/*
   describe ('Delete contents', () => {
     it('delete content and group', () => {
       cy.login('admin')
       cy.deleteContent(title)
-      cy.deleteContent(groupName)
+      cy.deleteContent(publicGroup)
+      cy.deleteContent(privateGroup)
     })
     users.forEach((user) => {
       it('delete user: ' + user.username,  () => {
@@ -180,5 +327,5 @@ context('Permissions', () => {
       })
     })
   })
+*/
 })
-
