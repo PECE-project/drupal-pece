8f51a598e9abbdd6d6a1fd05f1a73ef64795c2c8
Alan Ktquez <ktquez@gmail.com>
2019-12-29 11:45:29 -0300

    Update menu navigation

--- src/front/components/Navigation.vue
+++ src/front/components/Navigation.vue
@@ -8,6 +8,7 @@
         v-for="menu in menuHeader"
         :key="menu.label"
         class="p-6 py-5 md:px-4 lg:px-6 cursor-pointer hover:text-accent"
+        data-pece="menuHeader-items"
       >
         <a :href="menu.link">
           {{ menu.label }}
