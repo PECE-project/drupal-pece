9f08407c309646596e76b347f75b8f51577c760e
Alan Ktquez <ktquez@gmail.com>
2019-12-30 09:42:09 -0300

    Update main content page

--- src/front/pages/Home.vue
+++ src/front/pages/Home.vue
@@ -4,20 +4,25 @@
       <section class="highlights">
         <highlights />
       </section>
-      <section class="tabs">
-        <tabs />
+      <section class="tabs mt-6 md:mt-16">
+        <tabs :uppercase="true">
+          <tab :title="$t('recent_essays')">
+            <list-cards />
+          </tab>
+          <tab :title="$t('recent_artifacts')">
+            <list-cards />
+          </tab>
+        </tabs>
       </section>
-      <section class="about bg-gray-100 mt-6 md:mt-12 p-8 sm:p-12 flex flex-wrap lg:flex-no-wrap">
+      <section class="about bg-gray-100 mt-6 md:mt-16 p-8 sm:p-12 flex flex-wrap lg:flex-no-wrap">
         <div class="w-full lg:w-2/5">
           <img src="~/assets/images/logo-pece-ico.png" alt="Logo PECE project" class="w-12">
           <h1 class="mt-4 text-2xl lg:text-3xl font-bold sm:pr-2 leading-tight">
-            Platform for Experimental, Collaborative Ethnography
+            {{ $t('project_title') }}
           </h1>
         </div>
         <div class="w-full lg:w-3/5 text-sm">
-          <p class="my-6 lg:mt-16">
-            (PECE: pronounced “peace”) Is an open source (Drupal-based) digital platform that supports multi-sited, cross-scale ethnographic and historical research. The platform links researchers in new ways, enables new kinds of analyses and data visualization, and activates researchers’ engagement with public problems and diverse audiences. PECE is at the center of a research project that explores how digital infrastructure can be designed to support collaborative hermeneutics....
-          </p>
+          <p v-html="$t('project_description')" class="my-6 lg:mt-16" />
           <a href="#" class="link-accent-transparent uppercase text-xs font-bold">
             {{ $t('read_more') }}
           </a>
@@ -35,7 +40,9 @@ export default {
   name: 'Home',
   components: {
     Highlights: () => import(/* webpackChunkName: "Highlights" */ '@/components/Highlights'),
-    Tabs: () => import(/* webpackChunkName: "tabs" */ '@/components/Tabs')
+    Tabs: () => import(/* webpackChunkName: "tabs" */ '@/components/tabs/Tabs'),
+    Tab: () => import(/* webpackChunkName: "tab" */ '@/components/tabs/Tab'),
+    ListCards: () => import(/* webpackChunkName: "ListCards" */ '@/components/ListCards')
   }
 }
 </script>
