16c018d36ad8c32bbae67c63463b29c84aac0049
Alan Ktquez <ktquez@gmail.com>
2019-12-30 09:40:42 -0300

    Update translation

--- src/front/lang/en-US.js
+++ src/front/lang/en-US.js
@@ -1,10 +1,14 @@
 export default {
   description: 'Front-end for v2 PECE distribution',
+  project_title: 'Platform for Experimental, Collaborative Ethnography',
+  project_description: '(<b>PECE</b>: pronounced “peace”) Is an open source (Drupal-based) digital platform that supports multi-sited, cross-scale ethnographic and historical research. The platform links researchers in new ways, enables new kinds of analyses and data visualization, and activates researchers’ engagement with public problems and diverse audiences. PECE is at the center of a research project that explores how digital infrastructure can be designed to support collaborative hermeneutics....',
   search: 'Search',
   see: 'See',
   read_more: 'Read more',
   login: 'Log in',
   register: 'Register',
+  recent_essays: 'Recent essays',
+  recent_artifacts: 'Recent artifacts',
   a11y: {
     main_navigation: 'Main navigation',
     text_search: 'Search field to find content in site',
