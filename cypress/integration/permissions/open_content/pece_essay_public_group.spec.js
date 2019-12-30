e443b14af72ca4843a31b7842162ff5dfb60583f
Edmar Gomes <edmar.gomes@nyx.tc>
2019-12-29 22:54:46 -0300

    Split group tests to open content.

--- cypress/integration/permissions/open_content/pece_essay_public_group.spec.js
+++ cypress/integration/permissions/open_content/pece_essay_public_group.spec.js
@@ -79,9 +79,7 @@ context('Permissions', () => {
 
   })
 
-  describe('Group permissions', () => {
-
-    describe('Public group', () => {
+  describe('Public group', () => {
       describe('Default Visibility', () => {
         it ('group with default visibility', () => {
           cy.login('admin')
@@ -178,142 +176,6 @@ context('Permissions', () => {
     })
     })
 
-    describe('Private group', () => {
-
-      describe('Default Visibility', () => {
-        it ('group with default visibility', () => {
-          cy.login('admin')
-          cy.updateContent(title, [
-            '#edit-og-group-ref-und-0-default:select:' + privateGroup
-          ])
-        })
-        it("anonymous user can't access this content", () => {
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('authenticated user can\'t access this content', () => {
-          cy.login('user')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('researcher user can\'t access this content', () => {
-          cy.login('researcher')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('contributor user can access this content', () => {
-          cy.login('contributor')
-          cy.testAccess(path)
-          cy.testAccess(path + '/essay')
-        })
-
-        it('contributor user can access in panels editor', () => {
-          cy.login('contributor')
-          cy.visit(path + "/essay")
-          cy.contains('Customize this page').click()
-          cy.contains('Save as custom')
-          cy.contains('Cancel')
-        })
-
-        it("researcher user can't access this content", () => {
-          cy.login('researcher')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it ('add user in the content group', () => {
-          cy.login('admin')
-          cy.updateUser('user', [
-            '#edit-og-user-node-und-0-default:select:' + privateGroup
-          ])
-        })
-
-        it("user in group content can access this content", () => {
-          cy.login('user')
-          cy.testAccess(path)
-          cy.testAccess(path + '/essay')
-        })
-
-      })
-
-      describe('Public Visibility', () => {
-        it("change group visibility TO PUBLIC", () => {
-          cy.login('admin')
-          cy.updateContent(publicGroup, [
-            '#edit-group-content-access-und:select:Public - accessible to all site users'
-          ])
-        })
-        it("anonymous user can access this content", () => {
-          cy.testAccess(path)
-          cy.testAccess(path + '/essay')
-        })
-      })
-
-      describe('Private Visibility', () => {
-        it ("change group visibility TO PRIVATE", () => {
-          cy.login('admin')
-          cy.updateContent(privateGroup, [
-            '#edit-group-content-access-und:select:Private - accessible only to group members'
-          ])
-        })
-        it("anonymous user can't access this content", () => {
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('authenticated user can\'t access this content', () => {
-          cy.login('user')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('researcher user can\'t access this content', () => {
-          cy.login('researcher')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it('contributor user can access this content', () => {
-          cy.login('contributor')
-          cy.testAccess(path)
-          cy.testAccess(path + '/essay')
-        })
-
-        it('contributor user can access in panels editor', () => {
-          cy.login('contributor')
-          cy.visit(path + "/essay")
-          cy.contains('Customize this page').click()
-          cy.contains('Save as custom')
-          cy.contains('Cancel')
-        })
-
-        it("researcher user can't access this content", () => {
-          cy.login('researcher')
-          cy.testNoAccess(path)
-          cy.testNoAccess(path + '/essay')
-        })
-
-        it ('add researcher in the content group', () => {
-          cy.login('admin')
-          cy.updateUser('researcher', [
-            '#edit-og-user-node-und-0-default:select:' + privateGroup
-          ])
-        })
-
-        it("researcher user in group user can access this content", () => {
-          cy.login('researcher')
-          cy.testAccess(path)
-          cy.testAccess(path + '/essay')
-        })
-
-        //@TODO: Need test to add two groups, one public and visibility public other private and permission need keep public
-      })
-    })
-  })
-/*
   describe ('Delete contents', () => {
     it('delete content and group', () => {
       cy.login('admin')
@@ -327,5 +189,5 @@ context('Permissions', () => {
       })
     })
   })
-*/
+
 })
