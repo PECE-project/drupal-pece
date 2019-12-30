313daa4558df31e5afa3ecc5a5e3624ae49e78b3
Alan Ktquez <ktquez@gmail.com>
2019-12-30 09:41:48 -0300

    Add ListCard component

---
+++ src/front/components/ListCards.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="list-card flex flex-wrap justify-between">
+    <li v-for="index in Array(4)" :key="index" class="list-card__item pl-4 w-1/2 mb-4 md:w-1/4 md:mb-0">
+      <card />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListCards',
+  components: {
+    Card: () => import(/* webpackChunkName: "Card" */ '@/components/Card')
+  }
+}
+</script>
+
+<style lang="scss">
+.list-card {
+  margin-left: -1rem;
+}
+</style>
