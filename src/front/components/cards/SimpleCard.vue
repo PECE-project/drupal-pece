5774859ef687cd59c0937cff6aa2dccabfcd94ae
Alan Ktquez <ktquez@gmail.com>
2020-01-02 19:36:48 -0300

    Collaborate page

---
+++ src/front/components/cards/SimpleCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="simple-card rounded overflow-hidden shadow-pece">
+    <div class="card__media">
+      <a href="#">
+        <img src="https://picsum.photos/320/200" alt="Description image" class="w-full block">
+      </a>
+    </div>
+    <div class="card__content p-3 pb-4">
+      <h2 class="card__title text-sm font-bold text-gray-900 leading-tight">
+        <a href="#">
+          Open group
+        </a>
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
