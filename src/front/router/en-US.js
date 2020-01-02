e426eb5e06d2dd6c9b02b5509fef742cc08a9f90
Alan Ktquez <ktquez@gmail.com>
2020-01-02 15:52:04 -0300

    Add  navigation routes for avoid eslint error

--- src/front/router/en-US.js
+++ src/front/router/en-US.js
@@ -11,5 +11,20 @@ export default [
     path: '/about',
     name: 'about___en',
     component: About
+  },
+  {
+    path: '/collaborate',
+    name: 'collaborate___en',
+    component: About // Using component about avoid warning console
+  },
+  {
+    path: '/analyze',
+    name: 'analyze___en',
+    component: About
+  },
+  {
+    path: '/discover',
+    name: 'discover___en',
+    component: About
   }
 ]
--- src/front/router/pt-BR.js
+++ src/front/router/pt-BR.js
@@ -11,5 +11,20 @@ export default [
     path: '/pt/sobre',
     name: 'about___pt',
     component: About
+  },
+  {
+    path: '/pt/collaborate',
+    name: 'collaborate___pt',
+    component: About // Using component about avoid warning console
+  },
+  {
+    path: '/pt/analyze',
+    name: 'analyze___pt',
+    component: About
+  },
+  {
+    path: '/pt/discover',
+    name: 'discover___pt',
+    component: About
   }
 ]
