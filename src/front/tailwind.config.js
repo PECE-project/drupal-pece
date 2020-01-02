be7408bc822b27ee7d9e0180b7b008ef1b603142
Alan Ktquez <ktquez@gmail.com>
2020-01-02 06:33:04 -0300

    Add negative margin in tailwind config

--- src/front/tailwind.config.js
+++ src/front/tailwind.config.js
@@ -14,6 +14,13 @@ module.exports = {
       },
       boxShadow: {
         pece: '0 6px 4px #F7FAFC'
+      },
+      margin: {
+        '-px': '-1px',
+        '-1': '-0.25rem',
+        '-2': '-0.5rem',
+        '-3': '-0.75rem',
+        '-4': '-1rem'
       }
     }
   },
