fc589f2262a1453770f7b75596b4cf8415d32918
Alan Ktquez <ktquez@gmail.com>
2020-01-02 15:35:37 -0300

    Update routes

---
+++ src/front/router/en-US.js
@@ -0,0 +1,15 @@
+import About from '~/pages/About'
+import Home from '~/pages/Home'
+
+export default [
+  {
+    path: '/',
+    name: 'home___en',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about___en',
+    component: About
+  }
+]
---
+++ src/front/router/pt-BR.js
@@ -0,0 +1,15 @@
+import About from '~/pages/About'
+import Home from '~/pages/Home'
+
+export default [
+  {
+    path: '/pt',
+    name: 'home___pt',
+    component: Home
+  },
+  {
+    path: '/pt/sobre',
+    name: 'about___pt',
+    component: About
+  }
+]
