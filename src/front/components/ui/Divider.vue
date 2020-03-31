bd0e82d7b78f264899b99ac9357385f0a676f5be
Alan Ktquez <ktquez@gmail.com>
2020-03-31 01:04:22 -0300

    Add UI Divider component

---
+++ src/front/components/ui/Divider.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <hr :class="`border-${props.color} my-2`">
+</template>
+
+<script>
+export default {
+  name: 'Divider',
+
+  props: {
+    color: {
+      type: String,
+      default: 'gray-200'
+    }
+  }
+}
+</script>
