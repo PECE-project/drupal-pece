3dd3a814019fb4dde652c7faeb05c4125ce7cd57
Edmar Gomes <edmar.gomes@nyx.tc>
2020-04-04 19:16:59 -0300

    Add people api graphql.

---
+++ web/profiles/pece/module/pece_people/src/Plugin/GraphQL/DataProducer/TraitUser.php
@@ -0,0 +1,18 @@
+<?php
+namespace Drupal\pece_people\Plugin\GraphQL\DataProducer;
+
+use Drupal\paragraphs\Entity\Paragraph;
+
+trait TraitUser {
+    /**
+     * Mapping fields
+     *
+     */
+    protected $map_fields = [
+        'mail' => 'mail',
+        'pass' => 'pass',
+        'username' => 'name',
+        'status' => 'status',
+        'name' => 'field_name',
+    ];
+}
