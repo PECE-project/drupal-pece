a960c8264a366ef7bb1d9755ee0d30b80b2b6f59
Renato Vasconcellos <revagomes@gmail.com>
2020-05-05 04:31:34 -0300

    Update deploy script.

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -5,6 +5,6 @@ set -ev
 ls -lah
 
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-  ./build ubuntu@159.89.183.62:/home/ubuntu/drupal-pece/build
+  ./build ubuntu@159.89.183.62:/home/ubuntu/pece/build
 
 print "Deploy to DEV environment succeeded \o/"
