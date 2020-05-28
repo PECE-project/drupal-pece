88f3c13a6b32198e2f273c8ab8edb4299d964a14
Renato Vasconcellos <revagomes@gmail.com>
2020-05-27 22:13:16 -0300

    Upgrade Views Infinite Scroll module to v2.3.

--- profiles/pece/modules/contrib/views_infinite_scroll/views-infinite-scroll.js
+++ profiles/pece/modules/contrib/views_infinite_scroll/views-infinite-scroll.js
@@ -12,8 +12,8 @@
    */
   $.fn.infiniteScrollInsertView = function ($new_view) {
     var $existing_view = this;
-    var $existing_content = $existing_view.find('.view-content').children();
-    $new_view.find('.view-content').prepend($existing_content);
+    var $existing_content = $existing_view.find('> .view-content').last().children();
+    $new_view.find('> .view-content').last().first().prepend($existing_content);
     $existing_view.replaceWith($new_view);
     $(document).trigger('infiniteScrollComplete', [$new_view, $existing_content]);
   };
