36404b4479021b51e96d016173b4f5c6cbbc0c59
Edmar Gomes <edmar.gomes@nyx.tc>
2020-06-13 15:46:44 -0300

    Add graphql ready api to annotations.

---
+++ web/profiles/pece/module/pece_annotations/src/Plugin/GraphQL/SchemaExtension/AnnotationExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Drupal\pece_annotations\Plugin\GraphQL\SchemaExtension;
+
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistryInterface;
+use Drupal\nyx_graphql\Plugin\GraphQL\Schema\EntityExtension;
+
+/**
+ * @SchemaExtension(
+ *   id = "annotation_extension",
+ *   name = "Pece Annotation extension",
+ *   description = "A extension that adds Annotations related fields.",
+ *   schema = "entity"
+ * )
+ */
+class AnnotationExtension extends EntityExtension {
+
+  /**
+   * @inheritDoc
+   */
+  public function __construct($configuration, $pluginId, $pluginDefinition, ModuleHandlerInterface $moduleHandler) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition, $moduleHandler);
+    $this->entity = [
+      'type' => 'node',
+      'bundle' => 'pece_annotation',
+      'plural' => 'peceAnnotations'
+    ];
+  }
+
+  public function addFields(ResolverRegistryInterface $registry, ResolverBuilder $builder) {
+    parent::addFields($registry, $builder, ['annotation_']);
+  }
+
+}
