6985f55241acbb03bd6f87837c8292fdf6e083e7
Edmar Gomes <edmar.gomes@nyx.tc>
2022-05-15 17:36:28 -0300

    Add test to create pece artifact text and check permissions.

--- web/profiles/pece/modules/pece_artifacts/modules/pece_artifacts_text/tests/src/Functional/PeceArtifactTextTest.php
+++ web/profiles/pece/modules/pece_artifacts/modules/pece_artifacts_text/tests/src/Functional/PeceArtifactTextTest.php
@@ -27,26 +27,69 @@ class PeceArtifactTextTest extends BrowserTestBase {
     'update',
     'user',
     'views',
-    'workflow'
+    'workflow',
+    'pece_core',
   );
   protected $profile = 'pece';
   protected $strictConfigSchema = FALSE;
   protected $defaultTheme = "pece_theme";
 
   /**
-   * Setup and create content with Pbf field.
+   * Setup.
    */
   public function setUp() {
     parent::setUp();
   }
 
   /**
-   * Test the "pbf" node access with a Pbf field which reference node.
+   * Test the create node pece artifact text.
    */
   public function testCreateContent() {
     $this->artifactText1 = $this->drupalCreateNode(['type' => 'pece_artifact_text', 'title' => 'Artifact Text 1']);
     // Verify view access.
+    $this->drupalLogout();
     $this->drupalGet('node/'. $this->artifactText1->id());
+
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->titleEquals('Artifact Text 1 | Drupal');
+  }
+
+  /**
+   * Test the "pbf" node access with a Pbf field which reference node.
+   */
+  public function testNodeAccess() {
+    $user = $this->createUser();
+
+    $values = array(
+      'type' => 'pece_artifact_text',
+      'title' => 'Artifact Text 2',
+      'body' => [
+        'value' => 'Content body for Artifact Text 2',
+      ],
+    );
+
+    $values['field_pbc_ref_user'] = [
+      'target_id' => $user->id(),
+      'grant_public' => 1,
+      'grant_view' => 1,
+      'grant_update' => 1,
+      'grant_delete' => 1,
+    ];
+
+    $this->artifactText2 = $this->drupalCreateNode($values);
+
+    $this->drupalLogin($this->createUser([
+      'administer nodes',
+      'create pece_artifact_text content',
+      'edit any pece_artifact_text content'
+    ]));
+    $this->drupalGet('node/'. $this->artifactText2->id() . '/edit');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalLogout();
+    $this->drupalGet('node/'. $this->artifactText2->id());
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalLogin($user);
+    $this->drupalGet('node/'. $this->artifactText2->id());
     $this->assertSession()->statusCodeEquals(200);
   }
 
