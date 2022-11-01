e14e248a9af32b2cd283c760497ffcedff108de9
Reva Gomes <revagomes@gmail.com>
2022-11-02 00:37:34 +0100

    Fix broken urls in Nuxt.js image placeholders.

--- src/front/utils/fake.js
+++ src/front/utils/fake.js
@@ -86,7 +86,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/200/480/320.jpg'
+      url: 'https://picsum.photos/id/200/480/320.jpg'
     }
   },
   {
@@ -101,7 +101,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/96/480/320.jpg'
+      url: 'https://picsum.photos/id/96/480/320.jpg'
     }
   },
   {
@@ -128,7 +128,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/222/480/320.jpg'
+      url: 'https://picsum.photos/id/222/480/320.jpg'
     }
   },
   {
@@ -143,7 +143,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/180/480/320.jpg'
+      url: 'https://picsum.photos/id/180/480/320.jpg'
     }
   },
   {
@@ -158,7 +158,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/80/480/320.jpg'
+      url: 'https://picsum.photos/id/80/480/320.jpg'
     }
   },
   {
@@ -173,7 +173,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/20/480/320.jpg'
+      url: 'https://picsum.photos/id/20/480/320.jpg'
     }
   },
   {
@@ -188,7 +188,7 @@ export const simpleCardData = [
     },
     image: {
       alt: 'Image open group',
-      url: 'https://i.picsum.photos/id/269/480/320.jpg'
+      url: 'https://picsum.photos/id/269/480/320.jpg'
     }
   }
 ]
@@ -298,7 +298,7 @@ export const discoverData = [
     },
     image: {
       alt: 'Test group by researcher',
-      url: 'https://i.picsum.photos/id/200/480/320.jpg'
+      url: 'https://picsum.photos/id/200/480/320.jpg'
     }
   },
   {
@@ -360,9 +360,9 @@ export const highlights = [
     title: 'Platform for Experimental, Collaborative Ethnograph',
     images: {
       alt: 'teste',
-      thumbnail: 'https://i.picsum.photos/id/421/480/320.jpg',
-      medium: 'https://i.picsum.photos/id/421/640/480.jpg',
-      large: 'https://i.picsum.photos/id/421/1280/400.jpg'
+      thumbnail: 'https://picsum.photos/id/421/480/320.jpg',
+      medium: 'https://picsum.photos/id/421/640/480.jpg',
+      large: 'https://picsum.photos/id/421/1280/400.jpg'
     },
     link: 'https://google.com',
     created_at: 'Dezember 31, 2019'
@@ -371,9 +371,9 @@ export const highlights = [
     title: 'Green Mountain National Forest',
     images: {
       alt: 'teste',
-      thumbnail: 'https://i.picsum.photos/id/787/480/320.jpg',
-      medium: 'https://i.picsum.photos/id/787/640/480.jpg',
-      large: 'https://i.picsum.photos/id/787/1280/400.jpg'
+      thumbnail: 'https://picsum.photos/id/787/480/320.jpg',
+      medium: 'https://picsum.photos/id/787/640/480.jpg',
+      large: 'https://picsum.photos/id/787/1280/400.jpg'
     },
     link: 'https://facebook.com',
     created_at: 'April 26, 2019'
@@ -382,9 +382,9 @@ export const highlights = [
     title: 'The Amazon Rainforest',
     images: {
       alt: 'teste',
-      thumbnail: 'https://i.picsum.photos/id/442/480/320.jpg',
-      medium: 'https://i.picsum.photos/id/442/640/480.jpg',
-      large: 'https://i.picsum.photos/id/442/1280/400.jpg'
+      thumbnail: 'https://picsum.photos/id/442/480/320.jpg',
+      medium: 'https://picsum.photos/id/442/640/480.jpg',
+      large: 'https://picsum.photos/id/442/1280/400.jpg'
     },
     link: 'https://trello.com',
     created_at: 'November 05, 2019'
@@ -393,9 +393,9 @@ export const highlights = [
     title: 'Arashiyama Bamboo Grove',
     images: {
       alt: 'teste',
-      thumbnail: 'https://i.picsum.photos/id/454/480/320.jpg',
-      medium: 'https://i.picsum.photos/id/454/640/480.jpg',
-      large: 'https://i.picsum.photos/id/454/1280/400.jpg'
+      thumbnail: 'https://picsum.photos/id/454/480/320.jpg',
+      medium: 'https://picsum.photos/id/454/640/480.jpg',
+      large: 'https://picsum.photos/id/454/1280/400.jpg'
     },
     link: 'https://twitter.com',
     created_at: 'January 10, 2019'
