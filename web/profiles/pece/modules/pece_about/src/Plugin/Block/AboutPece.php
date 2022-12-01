61553d15a99392ee255e0e5f063e36f3ac5e1a9c
Reva Gomes <revagomes@gmail.com>
2022-12-01 21:08:10 +0100

    Fixes on PECE About module.

--- web/profiles/pece/modules/pece_about/src/Plugin/Block/AboutPece.php
+++ web/profiles/pece/modules/pece_about/src/Plugin/Block/AboutPece.php
@@ -3,6 +3,9 @@
 namespace Drupal\pece_about\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Extension\ExtensionPathResolver;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the AboutPece block.
@@ -12,7 +15,36 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("About PECE")
  * )
  */
-class AboutPece extends BlockBase {
+class AboutPece extends BlockBase implements ContainerFactoryPluginInterface {
+
+   /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('extension.path.resolver')
+    );
+  }
+
+  /**
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \Drupal\Core\Extension\ExtensionPathResolver $path_resolver
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ExtensionPathResolver $path_resolver) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->pathResolver = $path_resolver;
+  }
 
   /**
    * {@inheritdoc}
