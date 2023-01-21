bb1401be2b9b7db2c73b6f274a7a1873145db304
Reva Gomes <revagomes@gmail.com>
2023-01-21 19:41:12 +0100

    Refactoring HomeControler class.

--- lib/profiles/pece/modules/pece_home/src/Controller/HomeController.php
+++ lib/profiles/pece/modules/pece_home/src/Controller/HomeController.php
@@ -3,11 +3,14 @@
 namespace Drupal\pece_home\Controller;
 
 use \Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -60,7 +63,7 @@ class HomeController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('renderer'),
-      $container->get('entity_manager'),
+      $container->get('entity_type.manager'),
       $container->get('config.factory')
     );
   }
@@ -70,48 +73,100 @@ class HomeController extends ControllerBase {
    */
   public function buildPage() {
     $output = [];
-    $frontPgPath = $this->config
-      ->getEditable('system.site')
-      ->get('page.front');
+    $nid = $this->getFrontPageNid();
+    $node = $this->loadFrontNode($nid);
 
-    // Skip if no front page.
-    if (empty($frontPgPath)) {
+    if (empty($node)) {
       return $output;
     }
 
-    // Load and render the node.
+    return $this->entityTypeManager()
+      ->getViewBuilder('node')
+      ->view($node, 'default');
+  }
+
+  /**
+   * Get site front page config setting.
+   *
+   * @return array|mixed|null
+   */
+  private function getFrontPageSetting() {
+    return $this->config
+      ->getEditable('system.site')
+      ->get('page.front');
+  }
+
+  private function getFrontPageNid() {
+    $frontPgPath = $this->getFrontPageSetting();
+    // Skip if no front page.
+    if (empty($frontPgPath)) {
+      return FALSE;
+    }
+    // Extract node id from system.site.page.front setting.
     if (strpos($frontPgPath,'/node/') !== FALSE) {
       $nid = str_replace('/node/', '', $frontPgPath);
-      $node = $this->entityTypeManager()
-        ->getStorage('node')
-        ->load($nid);
-      return $this->entityTypeManager()
-        ->getViewBuilder('node')
-        ->view($node, 'default');
+      return $nid;
     }
+    else {
+      return FALSE;
+    }
+  }
+
+  /**
+   * Load node set as front page.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|false
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function loadFrontNode() {
+    $nid = $this->getFrontPageNid();
+    // Skip if node isn't available.
+    if (empty($nid)) {
+      return FALSE;
+    }
+
+    return $this->entityTypeManager()
+      ->getStorage('node')
+      ->load($nid);
   }
 
   /**
    * Check if a custom home page is created.
    */
-  public function isHomeCreated() {
-    return !empty($frontPgPath = $this->config
-      ->getEditable('system.site')
-      ->get('page.front'));
-  }
 
+  public function isHomeCreated() {
+    $nid = $this->getFrontPageNid();
+    // No front page is set.
+    if (empty($nid)) {
+      return FALSE;
+    }
+
+    // Is front page node published?
+    $nid = \Drupal::entityQuery('node')
+      ->accessCheck(TRUE)
+      ->condition('nid', $nid, '=')
+      ->condition('status', NodeInterface::PUBLISHED)
+      ->execute();
+    // There is a valid node.
+    if (!empty($nid)) {
+      return TRUE;
+    }
+
+    // Node is unpublished.
+    return FALSE;
+  }
   /**
    * Return PECE default home or a custom home page if available.
    */
   public function defaultHome() {
-    $output = $this->buildPage();
-    dsm($this->isHomeCreated(), 'Is Home created?');
-    dsm($output, 'Output');
     if  ($this->isHomeCreated()) {
-     $output = '';
+      return $this->buildPage();
     }
-
-    return $output;
+    // Redirect to front page as a fallback.
+    $url = Url::fromRoute('<front>');
+    $response = new RedirectResponse($url->toString());
+    $response->send();
   }
 
   /**
