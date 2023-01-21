f465c41dbf067242620e58c91ceaf8ab5d69b2cd
Reva Gomes <revagomes@gmail.com>
2023-01-21 13:05:11 +0100

    Improvements on PECE Theme styles structure.

--- lib/profiles/pece/modules/pece_home/src/Controller/HomeController.php
+++ lib/profiles/pece/modules/pece_home/src/Controller/HomeController.php
@@ -6,6 +6,8 @@ use \Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -21,37 +23,81 @@ class HomeController extends ControllerBase {
   protected $renderer;
 
   /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  /**
    * Creates a new HomeController instance.
    *
    * @param \Drupal\Core\Renderer\RendererInterface $date_formatter
-   *   The date formatter.
+   *   The renderer instance.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entity_manager
+   *   The entity manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $date_formatter
+   *   The config factory.
    */
-  public function __construct(RendererInterface $renderer) {
+  public function __construct(RendererInterface $renderer,
+                              EntityTypeManager $entity_manager,
+                              ConfigFactoryInterface $config_factory) {
     $this->renderer = $renderer;
+    $this->entityTypeManager = $entity_manager;
+    $this->config = $config_factory;
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('renderer'));
+    return new static(
+      $container->get('renderer'),
+      $container->get('entity_manager'),
+      $container->get('config.factory')
+    );
   }
 
   /**
    * Build default Home page.
    */
   public function buildPage() {
-    return [
-      '#type' => 'markup',
-      '#markup' => 'Home page',
-    ];
+    $output = [];
+    $frontPgPath = $this->config
+      ->getEditable('system.site')
+      ->get('page.front');
+
+    // Skip if no front page.
+    if (empty($frontPgPath)) {
+      return $output;
+    }
+
+    // Load and render the node.
+    if (strpos($frontPgPath,'/node/') !== FALSE) {
+      $nid = str_replace('/node/', '', $frontPgPath);
+      $node = $this->entityTypeManager()
+        ->getStorage('node')
+        ->load($nid);
+      return $this->entityTypeManager()
+        ->getViewBuilder('node')
+        ->view($node, 'default');
+    }
   }
 
   /**
    * Check if a custom home page is created.
    */
   public function isHomeCreated() {
-    return FALSE;
+    return !empty($frontPgPath = $this->config
+      ->getEditable('system.site')
+      ->get('page.front'));
   }
 
   /**
@@ -59,7 +105,8 @@ class HomeController extends ControllerBase {
    */
   public function defaultHome() {
     $output = $this->buildPage();
-
+    dsm($this->isHomeCreated(), 'Is Home created?');
+    dsm($output, 'Output');
     if  ($this->isHomeCreated()) {
      $output = '';
     }
@@ -76,7 +123,7 @@ class HomeController extends ControllerBase {
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function access(AccountInterface $account) {
+  public function accessCheck($account) {
     // Check permissions. Pass forward parameters from the route and/or request
     // as needed.
     return AccessResult::allowedIf($account->hasPermission('access content'));
