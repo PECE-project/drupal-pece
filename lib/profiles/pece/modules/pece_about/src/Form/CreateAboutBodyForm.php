01c1772f1ddb88b2c082d29f9f936b02192d1c21
Reva Gomes <revagomes@gmail.com>
2023-01-24 01:30:18 +0100

    Uppdate Create About page body config form.

--- lib/profiles/pece/modules/pece_about/src/Form/CreateAboutBodyForm.php
+++ lib/profiles/pece/modules/pece_about/src/Form/CreateAboutBodyForm.php
@@ -2,81 +2,90 @@
 
 namespace Drupal\pece_about\Form;
 
-use Drupal\Core\Form\ConfirmFormBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
-use Exception;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Builds the form to create About.
+ * Builds the form to create About page content.
  *
  * @codeCoverageIgnore
  */
-class CreateAboutBodyForm extends ConfirmFormBase
-{
+class CreateAboutBodyForm extends ConfigFormBase {
+
+  /**
+   * Constructs a CreateAboutBodyForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    parent::__construct($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'pece_about_create_about_body_form';
+    return 'pece_about_create_about_body';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['pece_about.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['body'] = [
+    $config = $this->config('pece_about.settings');
+    $form['content'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Content'),
+      '#open' => TRUE,
+    );
+    $form['content']['about'] = [
       '#type' => 'text_format',
+      '#title' => $this->t('About page'),
       '#attributes' => [
         'class' => ['body']
-      ]
+      ],
+      '#default_value' => $config->get('body'),
+      '#maxlength' => NULL,
     ];
-    
     return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getQuestion()
-  {
-    return $this->t('Enter the content for your about page:');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCancelUrl()
-  {
-    return new Url('pece_about.admin_config_pece_about');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfirmText()
-  {
-    return $this->t('Create');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
-    $body = $form_state->getValue("body");
-    $config = \Drupal::service('config.factory')->getEditable('about.settings');
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $body = $form_state->getValue("about");
+    $config = $this->configFactory->getEditable('pece_about.settings');
     $config->set('body', $body['value'])->save();
-    $this->messenger()->addMessage($this->t('The about text has been saved.', []));
+
+    parent::submitForm($form, $form_state);
   }
 
 }
