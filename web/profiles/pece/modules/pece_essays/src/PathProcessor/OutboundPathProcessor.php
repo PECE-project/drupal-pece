1afb3680dd0eb5946186f8cb8702342b82feb1af
benjamin melançon <ben@agaric.coop>
2024-10-26 01:13:41 -0400

    Made essay intro page show correct view mode (actually make links go to the intro page
    All this dancing about is caused by Layout Builder insisting on taking over the default view mode.
    
    Ref #466

---
+++ web/profiles/pece/modules/pece_essays/src/PathProcessor/OutboundPathProcessor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\pece_essays\PathProcessor;
+
+use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
+use Drupal\Core\Render\BubbleableMetadata;
+use Symfony\Component\HttpFoundation\Request;
+
+class OutboundPathProcessor implements OutboundPathProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processOutbound($path, &$options = [], Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
+    if (!isset($options['entity']) || $options['route']->getPath() !== "/node/{node}" || ! $options['entity'] instanceof \Drupal\node\NodeInterface) {
+      return $path;
+    }
+    // We are linking to a node at its canonical path.
+    /** @var Drupal\node\NodeInterface $node */
+    $node = $options['entity'];
+    if ($node->bundle() === "pece_essay") {
+      return $path . '/intro';
+    }
+    return $path;
+  }
+}
