f64225d01a97b315cd58c38f4028d0ed8fc48b27
Keegan Rankin <keegan@agaric.coop>
2024-11-03 19:37:30 -0800

    Add jQuery to select legacy tabs

---
+++ web/themes/custom/peceful/skins/default/js/legacy.js
@@ -0,0 +1,22 @@
+(function ($, Drupal, once) {
+  Drupal.behaviors.peceTabsLegacy = {
+    attach: function (context, settings) {
+      $(once('peceTabsLegacy', '.legacy-rendered-html')).each(function (element) {
+        $(this).find('#tabs-0-column1 a').on('click', function(e) {
+          let href = $(this).attr("href");
+          alert(href);
+        })
+      })
+    }
+  }
+})(jQuery, Drupal, once);
+
+
+// function h() {
+//   return i || r.options.disabled !== !0 && !e(this).hasClass("ui-state-disabled") ? ("string" == typeof o ? r[o] : o).apply(r, arguments) : t
+// }
+
+// function(a) {
+//   return "undefined" == typeof n || a && n.event.triggered === a.type ? void 0 : n.event.dispatch.apply(k.elem, arguments)
+// }
+
