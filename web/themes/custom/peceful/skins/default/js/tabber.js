96d4b70a6707f5da55834bb335c3aebb9ec7852f
dave onion <onion@radicante.media>
2024-11-05 14:09:06 -0500

    Separating tabs out in columns to treat each column independently

--- web/themes/custom/peceful/skins/default/js/tabber.js
+++ web/themes/custom/peceful/skins/default/js/tabber.js
@@ -1,33 +1,40 @@
-// This js is attached as a library to node--pece-essay--full.html.twig to handle 
-// scraped and imported content with tabs and corresponding divs
+// This script manages tab behavior in node--pece-essay--full.html.twig,
+// where content is structured in tabs with associated divs.
 
-const tabLinks = document.querySelectorAll('ul li [href^="#tabs-"]');
-const tabContainers = document.querySelectorAll('.item-list');
+const tabColumns = document.querySelectorAll(".panel-panel-inner > [id^=tabs-0-column]");
 
-// Add bulma tab classes to the containers 
-tabContainers.forEach(tabContainer => {
-  tabContainer.classList.add("tabs", "is-boxed", "is-left", "is-small");  
-});
+tabColumns.forEach((tabColumn) => {
+  const tabLinks = tabColumn.querySelectorAll('ul li [href^="#tabs-"]');
+  const tabContainers = tabColumn.querySelectorAll(".item-list");
 
-let tabDivs = [];
-
-tabLinks.forEach(tabLink => {  
-  const linkedDiv = document.querySelector(tabLink.hash);
-  tabDivs.push(linkedDiv);
-  linkedDiv.classList.add('hidden');
-
-  tabLink.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    tabDivs.forEach(tabDiv => {
-      tabDiv.classList.add('hidden');
-    });
-    
-    tabLinks.forEach( nonActiveLink => {
-      nonActiveLink.parentNode.classList.remove("is-active");
-    }); 
-
-    linkedDiv.classList.remove('hidden');
-    tabLink.parentNode.classList.add("is-active");
+  // Add bulma tab classes to the container
+  tabContainers.forEach((container) => {
+    container.classList.add("tabs", "is-boxed", "is-left", "is-small");
   });
+
+  const tabDivs = Array.from(tabLinks).map(link => {
+    const contentDiv = tabColumn.querySelector(link.hash);
+    contentDiv.classList.add("hidden");
+    return contentDiv;
+  });
+
+  tabColumn.addEventListener("click", (event) => {
+    const clickedLink = event.target.closest('[href^="#tabs-"]');
+    if (!clickedLink) return; // Ignore clicks outside of tab links
+    event.preventDefault();
+
+    // Deactive all the tabs
+    tabDivs.forEach(div => div.classList.add("hidden"));
+    tabLinks.forEach(link => link.parentNode.classList.remove("is-active"));
+
+    const targetDiv = tabColumn.querySelector(clickedLink.hash);
+    targetDiv.classList.remove("hidden");
+    clickedLink.parentNode.classList.add("is-active");
+  });
+
+  // Make the first of each active
+  const firstTabLink = tabLinks[0];
+  const firstTabDiv = tabDivs[0];
+  firstTabLink.parentNode.classList.add("is-active");
+  firstTabDiv.classList.remove("hidden");
 });
