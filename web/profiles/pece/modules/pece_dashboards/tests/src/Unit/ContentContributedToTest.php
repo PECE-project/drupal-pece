e21d3590c4b9c2300b5bd3e2a788948a45e6a7ac
Reva Gomes <revagomes@gmail.com>
2026-06-03 23:43:02 +0200

    test(pece_dashboards): add unit tests for ContentContributedTo block

---
+++ web/profiles/pece/modules/pece_dashboards/tests/src/Unit/ContentContributedToTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\pece_dashboards\Unit;
+
+use Drupal\Core\Cache\Context\CacheContextsManager;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\pece_dashboards\Plugin\Block\ContentContributedTo;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\pece_dashboards\Plugin\Block\ContentContributedTo
+ * @group pece_dashboards
+ */
+class ContentContributedToTest extends UnitTestCase {
+
+  protected ContentContributedTo $block;
+
+  protected function setUp(): void {
+    parent::setUp();
+
+    $account = $this->createMock(AccountProxyInterface::class);
+    $account->method('id')->willReturn(42);
+
+    $cacheContextsManager = $this->createMock(CacheContextsManager::class);
+    $cacheContextsManager->method('assertValidTokens')->willReturn(TRUE);
+
+    $container = new ContainerBuilder();
+    $container->set('current_user', $account);
+    $container->set('string_translation', $this->getStringTranslationStub());
+    $container->set('cache_contexts_manager', $cacheContextsManager);
+    \Drupal::setContainer($container);
+
+    $this->block = new ContentContributedTo([], 'pece_dashboards_content_contributed_to', ['provider' => 'pece_dashboards']);
+  }
+
+  /**
+   * @covers ::getCacheContexts
+   */
+  public function testCacheContextsIncludeUser(): void {
+    $this->assertContains('user', $this->block->getCacheContexts());
+  }
+
+  /**
+   * @covers ::build
+   */
+  public function testBuildReturnsBuildArrayWithButtonKey(): void {
+    $build = $this->block->build();
+    $this->assertArrayHasKey('button', $build);
+    $this->assertEquals('component', $build['button']['#type']);
+    $this->assertEquals('pece_dashboards:button', $build['button']['#component']);
+  }
+
+  /**
+   * @covers ::build
+   */
+  public function testBuildUrlContainsCurrentUserId(): void {
+    $build = $this->block->build();
+    $this->assertStringContainsString('contributor[0]=42', $build['button']['#props']['url']);
+  }
+
+}
+
+// Define base_path() in the source class namespace so unit tests can call it
+// without a full Drupal bootstrap.
+namespace Drupal\pece_dashboards\Plugin\Block;
+
+if (!function_exists('Drupal\pece_dashboards\Plugin\Block\base_path')) {
+  function base_path(): string {
+    return '/';
+  }
+}
