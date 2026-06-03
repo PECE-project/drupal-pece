8cec8f53f8c48a6b6959f4fd0603e6a0f16b2a6f
Reva Gomes <revagomes@gmail.com>
2026-06-04 00:03:13 +0200

    test(pece_artifacts_audio): add functional tests for audio subtype
    Aligns PeceArtifactAudioTest with the canonical pattern used by image,
    video, pdf, and website subtypes: bracket array syntax, typed return on
    test methods, node module in \$modules list, and simplified testNodeAccess
    using owner-only access assertion.

---
+++ web/profiles/pece/modules/pece_artifacts/modules/pece_artifacts_audio/tests/src/Functional/PeceArtifactAudioTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\Tests\pece_artifacts_audio\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test create artifact Audio.
+ *
+ * @group pece_artifacts
+ * @group pece_artifacts_audio
+ */
+class PeceArtifactAudioTest extends BrowserTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'pece_artifacts_audio',
+    'node',
+    'components',
+    'token',
+    'typed_data',
+    'update',
+    'user',
+    'views',
+  ];
+
+  protected $profile = 'pece';
+  protected $strictConfigSchema = FALSE;
+  protected $defaultTheme = 'peceful';
+
+  /**
+   * Test the create node pece artifact audio.
+   */
+  public function testCreateContent(): void {
+    $node = $this->drupalCreateNode([
+      'type'  => 'pece_artifact_audio',
+      'title' => 'Audio Artifact 1',
+    ]);
+
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->titleEquals('Audio Artifact 1 | Drupal');
+  }
+
+  /**
+   * Test the "pbf" node access with a Pbf field which reference node.
+   */
+  public function testNodeAccess(): void {
+    $owner = $this->createUser();
+
+    $node = $this->drupalCreateNode([
+      'type'               => 'pece_artifact_audio',
+      'title'              => 'Audio Artifact 2',
+      'field_pbc_ref_user' => [
+        'target_id'    => $owner->id(),
+        'grant_public' => 0,
+        'grant_view'   => 1,
+        'grant_update' => 1,
+        'grant_delete' => 1,
+      ],
+    ]);
+
+    $this->drupalLogout();
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(403);
+
+    $this->drupalLogin($owner);
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
