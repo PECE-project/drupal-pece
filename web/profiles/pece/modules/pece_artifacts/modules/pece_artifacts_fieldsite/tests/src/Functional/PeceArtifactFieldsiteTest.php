4478b70c5108b439f9c394d0b069775715d24eaf
Reva Gomes <revagomes@gmail.com>
2026-06-04 00:09:39 +0200

    test(pece_artifacts): add functional tests for bundle and fieldsite subtypes

---
+++ web/profiles/pece/modules/pece_artifacts/modules/pece_artifacts_fieldsite/tests/src/Functional/PeceArtifactFieldsiteTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\Tests\pece_artifacts_fieldsite\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test create artifact fieldsite.
+ *
+ * @group pece_artifacts
+ * @group pece_artifacts_fieldsite
+ */
+class PeceArtifactFieldsiteTest extends BrowserTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'pece_artifacts_fieldsite',
+    'components',
+    'file',
+    'node',
+    'token',
+    'typed_data',
+    'update',
+    'user',
+    'views',
+  ];
+
+  protected $profile = 'pece';
+  protected $strictConfigSchema = FALSE;
+  protected $defaultTheme = 'peceful';
+
+  /**
+   * Test the create node pece artifact fieldsite.
+   */
+  public function testCreateContent(): void {
+    $node = $this->drupalCreateNode([
+      'type'  => 'pece_fieldsite',
+      'title' => 'Fieldsite Artifact 1',
+    ]);
+
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->titleEquals('Fieldsite Artifact 1 | Drupal');
+  }
+
+  /**
+   * Test the "pbf" node access with a Pbf field which reference node.
+   */
+  public function testNodeAccess(): void {
+    $owner = $this->createUser();
+
+    $node = $this->drupalCreateNode([
+      'type'               => 'pece_fieldsite',
+      'title'              => 'Fieldsite Artifact 2',
+      'field_pbc_ref_user' => [
+        'target_id'    => $owner->id(),
+        'grant_public' => 0,
+        'grant_view'   => 1,
+        'grant_update' => 1,
+        'grant_delete' => 1,
+      ],
+    ]);
+
+    $this->drupalResetSession();
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(403);
+
+    $this->drupalLogin($owner);
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
